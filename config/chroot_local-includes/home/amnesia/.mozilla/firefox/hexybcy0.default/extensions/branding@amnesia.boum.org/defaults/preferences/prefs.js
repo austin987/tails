10896f237c500e3b92737ba935c2004d7e49a4bb
amnesia <amnesia@boum.org>
2009-11-22 18:37:50 +0100

    $HOME(iceweasel): install custom extension to localize search engine at runtime
    ... instead of editing user.js at build-time. Still to be tested.

---
+++ config/chroot_local-includes/home/amnesia/.mozilla/firefox/hexybcy0.default/extensions/branding@amnesia.boum.org/defaults/preferences/prefs.js
@@ -0,0 +1,2 @@
+pref("browser.search.defaultenginename", "chrome://amnesiabranding/locale/amnesia.properties");
+pref("browser.search.selectedEngine", "chrome://amnesiabranding/locale/amnesia.properties");
