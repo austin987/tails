9e06021e6162a31e69bc658956042c9abf1968ac
amnesia <amnesia@boum.org>
2010-02-13 14:14:05 +0100

    Don't run the Tor NetworkManager hook when configuring the loopback interface.

--- config/chroot_local-includes/etc/NetworkManager/dispatcher.d/60-tor-sighup.sh
+++ config/chroot_local-includes/etc/NetworkManager/dispatcher.d/60-tor-sighup.sh
@@ -1,5 +1,10 @@
 #! /bin/sh
 
+# Run only when the interface is not "lo":
+if [[ $1 = "lo" ]]; then
+   exit 0
+fi
+
 # Run whenever an interface gets "up", not otherwise:
 if [[ $2 != "up" ]]; then
    exit 0
