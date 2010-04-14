949d6a0f8da3f2b6a1c85cc20a897b51e816c6f7
amnesia <amnesia@boum.org>
2010-04-14 23:26:08 +0200

    Configure iceweasel profile skeleton in /etc/iceweasel.
    **UNTESTED**
    
    A brand new profile is setup from this skeleton once iceweasel is started
    after boot. Got rid of the last binary sqlite file (namely places.sqlite)
    in our source, and of various hard-to-understand autogenerated stuff.
    
    Moved the GNOME localization script (Nautilus secure delete, Scroogle search
    engine) to a live-bottom script: the user's iceweasel profile has not been
    created yet when GNOME starts, so we could not install the localize search
    engine into her profile at this point => better install it globally earlier in
    the boot sequence.

---
+++ config/chroot_local-includes/etc/iceweasel/pref/iceweasel.js
@@ -0,0 +1,117 @@
+// This is the Debian specific preferences file for Iceweasel
+// You can make any change in here, it is the purpose of this file.
+// You can, with this file and all files present in the
+// /etc/iceweasel/pref directory, override any preference that is
+// present in /usr/lib/iceweasel/defaults/preferences directory.
+// While your changes will be kept on upgrade if you modify files in
+// /etc/iceweasel/pref, please note that they won't be kept if you
+// do make your changes in /usr/lib/iceweasel/defaults/preferences.
+//
+// Note that lockPref is allowed in these preferences files if you
+// don't want users to be able to override some preferences.
+
+// Use LANG environment variable to choose locale
+pref("intl.locale.matchOS", true);
+
+// Disable default browser checking.
+pref("browser.shell.checkDefaultBrowser", false);
+
+pref("app.update.auto", false);
+pref("app.update.disable_button.showUpdateHistory", false);
+pref("app.update.enabled", false);
+pref("browser.cache.disk.enable", false);
+pref("browser.download.manager.closeWhenDone", true);
+pref("browser.download.manager.retention", 0);
+pref("browser.history_expire_days", 0);
+pref("browser.safebrowsing.enabled", false);
+pref("browser.safebrowsing.malware.enabled", false);
+pref("browser.safebrowsing.remoteLookups", false);
+pref("browser.search.suggest.enabled", false);
+pref("browser.search.update", false);
+pref("browser.startup.homepage_override.mstone", "ignore");
+pref("capability.policy.maonoscript.javascript.enabled", "allAccess");
+pref("capability.policy.maonoscript.sites", "https://auk.riseup.net https://mail.riseup.net https://swift.riseup.net https://tern.riseup.net https://webmail.no-log.org about: about:blank about:certerror about:config about:credits about:neterror about:plugins about:privatebrowsing about:sessionrestore chrome: file:// https://webmail.boum.org resource:");
+pref("extensions.adblockplus.synchronizationinterval", 8766);
+pref("extensions.firegpg.activate_inline", false);
+pref("extensions.firegpg.assisant_has_already_been_executed", true);
+pref("extensions.firegpg.enable_gpgapi", false);
+pref("extensions.firegpg.gmail_enabled", false);
+pref("extensions.firegpg.gmail_enabled", false);
+pref("extensions.firegpg.keyserver", "pool.sks-keyservers.net");
+pref("extensions.firegpg.show_website", false);
+pref("extensions.torbutton.block_tforms", false);
+pref("extensions.torbutton.clear_cookies", true);
+pref("extensions.torbutton.cookie_jars", false);
+pref("extensions.torbutton.http_port", 8118);
+pref("extensions.torbutton.http_proxy", "localhost");
+pref("extensions.torbutton.https_port", 8118);
+pref("extensions.torbutton.https_proxy", "localhost");
+pref("extensions.torbutton.locked_mode", true);
+pref("extensions.torbutton.no_updates", true);
+pref("extensions.torbutton.noncrashed", true);
+pref("extensions.torbutton.nonontor_sessionstore", true);
+pref("extensions.torbutton.nontor_memory_jar", true);
+pref("extensions.torbutton.saved.download_retention", 0);
+pref("extensions.torbutton.saved.search_suggest", false);
+pref("extensions.torbutton.saved.socks_remote_dns", true);
+pref("extensions.torbutton.saved.socks_version", 5);
+pref("extensions.torbutton.saved.type", 5);
+pref("extensions.torbutton.saved.share_proxy_settings", false);
+pref("extensions.torbutton.socks_host", "127.0.0.1");
+pref("extensions.torbutton.socks_port", 9050);
+pref("extensions.torbutton.spoof_english", false);
+pref("extensions.torbutton.startup", true);
+pref("extensions.torbutton.startup_state", 1);
+pref("extensions.torbutton.tor_enabled", true);
+pref("extensions.torbutton.tor_memory_jar", true);
+pref("extensions.torbutton.warned_ff3", true);
+pref("extensions.update.enabled", false);
+pref("extensions.update.notifyUser", false);
+pref("network.cookie.lifetimePolicy", 2);
+pref("network.cookie.prefsMigrated", true);
+pref("network.protocol-handler.warn-external.file", true);
+pref("network.protocol-handler.warn-external.mailto", true);
+pref("network.protocol-handler.warn-external.news", true);
+pref("network.protocol-handler.warn-external.nntp", true);
+pref("network.protocol-handler.warn-external.snews", true);
+pref("network.proxy.http", "localhost");
+pref("network.proxy.http_port", 8118);
+pref("network.proxy.socks", "127.0.0.1");
+pref("network.proxy.socks_port", 9050);
+pref("network.proxy.socks_remote_dns", true);
+pref("network.proxy.ssl", "localhost");
+pref("network.proxy.ssl_port", 8118);
+pref("network.proxy.type", 1);
+pref("network.security.ports.banned", "8118,8123,9050,9051");
+pref("layout.spellcheckDefault", 0);
+pref("noscript.httpsForced", "boum.org\nmail.google.com\nmail.riseup.net\nwebmail.no-log.org\nwebmail.boum.org");
+pref("noscript.httpsForcedExceptions", "");
+pref("noscript.notify.hide", true);
+pref("noscript.policynames", "");
+pref("noscript.showAddress", true);
+pref("noscript.showAllowPage", false);
+pref("noscript.showDistrust", true);
+pref("noscript.showDomain", true);
+pref("noscript.showGlobal", false);
+pref("noscript.showPermanent", false);
+pref("noscript.showTempToPerm", false);
+pref("noscript.showUntrusted", true);
+pref("noscript.untrusted", "google-analytics.com google.com file:// http://google-analytics.com http://google.com https://google-analytics.com https://google.com");
+pref("pref.privacy.disable_button.cookie_exceptions", false);
+pref("pref.privacy.disable_button.view_cookies", false);
+pref("pref.privacy.disable_button.view_passwords", false);
+pref("privacy.item.cookies", true);
+pref("privacy.item.offlineApps", true);
+pref("privacy.item.passwords", true);
+pref("privacy.sanitize.didShutdownSanitize", true);
+pref("privacy.sanitize.promptOnSanitize", false);
+pref("privacy.sanitize.sanitizeOnShutdown", true);
+pref("security.disable_button.openCertManager", false);
+pref("security.enable_java", false);
+pref("security.enable_ssl2", false);
+pref("security.enable_ssl3", true);
+pref("security.enable_tls", true);
+pref("security.warn_leaving_secure", true);
+pref("security.warn_submit_insecure", true);
+pref("signon.prefillForms", false);
+pref("signon.rememberSignons", false);
