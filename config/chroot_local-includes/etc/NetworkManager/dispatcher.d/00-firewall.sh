bb611b8412a91cb8260cc793f5f6a0c507be179e
amnesia <amnesia@boum.org>
2010-04-30 15:52:26 +0200

    firewall: forbid any IPv6 traffic with the outside.

--- config/chroot_local-includes/etc/NetworkManager/dispatcher.d/00-firewall.sh
+++ config/chroot_local-includes/etc/NetworkManager/dispatcher.d/00-firewall.sh
@@ -6,9 +6,15 @@ if [[ $2 != "up" ]]; then
 fi
 
 IPTABLES_RULES=/etc/firewall.conf
+IP6TABLES_RULES=/etc/firewall6.conf
 
 [ -x /sbin/iptables-restore ]	|| exit 2
 [ -n "$IPTABLES_RULES" ]	|| exit 3
 [ -r "$IPTABLES_RULES" ]	|| exit 4
 
+[ -x /sbin/ip6tables-restore ]	|| exit 12
+[ -n "$IP6TABLES_RULES" ]	|| exit 13
+[ -r "$IP6TABLES_RULES" ]	|| exit 14
+
 /sbin/iptables-restore < "$IPTABLES_RULES"
+/sbin/ip6tables-restore < "$IP6TABLES_RULES"
