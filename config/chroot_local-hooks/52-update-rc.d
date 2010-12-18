4d833ae9034f51e40e4306f42b2bea409d50ccb8
T(A)ILS developers <amnesia@boum.org>
2010-12-18 19:21:45 +0100

    Fix update-rc.d invocation syntax.

--- config/chroot_local-hooks/52-update-rc.d
+++ config/chroot_local-hooks/52-update-rc.d
@@ -7,4 +7,4 @@ update-rc.d tails-detect-virtualization defaults
 update-rc.d tails-wifi defaults
 
 # we run Tor ourselves after HTP via NetworkManager hooks
-update-rc.d disable tor
+update-rc.d tor disable
