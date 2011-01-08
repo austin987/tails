75086d496a36826f874a065760fc16b4109ebba1
T(A)ILS developers <amnesia@boum.org>
2011-01-08 19:29:48 +0100

    Disable init system's concurrency at the end of the boot sequence.
    Dependency based boot sequencing is now the default in Debian.
    
    We want to benefit from it to decrease boot time,
    but we need to disable it for the shutdown sequence in order to have our memory
    wiping hooks working in a reliable way... or at least to help fixing their bugs.

--- config/chroot_local-hooks/52-update-rc.d
+++ config/chroot_local-hooks/52-update-rc.d
@@ -4,6 +4,7 @@ echo "managing initscripts"
 
 # enable custom initscripts
 update-rc.d tails-detect-virtualization defaults
+update-rc.d tails-disable-init-concurrency defaults
 update-rc.d tails-wifi defaults
 
 # we run Tor ourselves after HTP via NetworkManager hooks
