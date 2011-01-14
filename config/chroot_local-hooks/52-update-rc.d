162f0d695078525072ce93ed9c5bd54e4caf1a27
T(A)ILS developers <amnesia@boum.org>
2011-01-14 12:28:02 +0100

    Cleanup custom initscripts runlevels setup.

--- config/chroot_local-hooks/52-update-rc.d
+++ config/chroot_local-hooks/52-update-rc.d
@@ -3,9 +3,9 @@
 echo "managing initscripts"
 
 # enable custom initscripts
-update-rc.d tails-detect-virtualization defaults
+update-rc.d tails-detect-virtualization 17 S .
 update-rc.d tails-disable-init-concurrency defaults
-update-rc.d tails-wifi defaults
+update-rc.d tails-wifi 17 S .
 
 # we run Tor ourselves after HTP via NetworkManager hooks
 update-rc.d tor disable
