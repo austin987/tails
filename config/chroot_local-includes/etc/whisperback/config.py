bd87622011abdf1fac5bb47a713eb5a4020cf983
T(A)ILS developers <amnesia@boum.org>
2011-03-02 01:37:48 +0100

    config/ and debian/: T(A)ILS has been renamed to Tails.

--- config/chroot_local-includes/etc/whisperback/config.py
+++ config/chroot_local-includes/etc/whisperback/config.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 #
-# T(A)ILS configuration file for Whisperback
+# Tails configuration file for Whisperback
 # ==========================================
 #
 # This is a python script that will be read at startup. Any python
@@ -96,7 +96,7 @@ def mail_prepended_info():
     except subprocess.CalledProcessError:
       amnesia_version = "tails-version returned an error"
     
-    return "T(A)ILS-Version: %s\n" % amnesia_version
+    return "Tails-Version: %s\n" % amnesia_version
 
 # A callback function to get information to append to the email
 # (this information will be encrypted). This is useful to add
