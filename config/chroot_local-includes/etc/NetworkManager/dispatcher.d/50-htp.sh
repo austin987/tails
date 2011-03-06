1165e5e0b7fa6a6ec2571f7d538dad1185872e5d
Tails developers <amnesia@boum.org>
2011-03-06 23:00:19 +0100

    Make dispatcher.d/50-htp.sh POSIX compatible
    Also make the code look more like idiomatic shell scripting.

--- config/chroot_local-includes/etc/NetworkManager/dispatcher.d/50-htp.sh
+++ config/chroot_local-includes/etc/NetworkManager/dispatcher.d/50-htp.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Rationale: Tor needs a somewhat accurate clock to work, and for that
 # HTP is currently the only practically usable solution when one wants
@@ -33,19 +33,18 @@ LOG=/var/log/htpdate.log
 DONE_FILE=/var/lib/live/htp-done
 SUCCESS_FILE=/var/lib/live/htp-success
 
-declare -a HTP_POOL
-HTP_POOL=(
-	'www.torproject.org'
-	'mail.riseup.net'
-	'encrypted.google.com'
-	'ssl.scroogle.org'
-)
+HTP_POOL="
+	www.torproject.org
+	mail.riseup.net
+	encrypted.google.com
+	ssl.scroogle.org
+"
 
 BEGIN_MAGIC='### BEGIN HTP HOSTS'
 END_MAGIC='### END HTP HOSTS'
 
-if [[ -n "${DHCP4_DOMAIN_NAME_SERVERS}" ]]; then
-	NAME_SERVERS="${DHCP4_DOMAIN_NAME_SERVERS}"
+if [ -n "$DHCP4_DOMAIN_NAME_SERVERS" ]; then
+	NAME_SERVERS="$DHCP4_DOMAIN_NAME_SERVERS"
 else
 	NAME_SERVERS="208.67.222.222 208.67.220.220"
 fi
@@ -54,32 +53,32 @@ fi
 ### Exit conditions
 
 # Run only when the interface is not "lo":
-if [[ $1 = "lo" ]]; then
+if [ "$1" = "lo" ]; then
 	exit 0
 fi
 
 # Run whenever an interface gets "up", not otherwise:
-if [[ $2 != "up" ]]; then
+if [ "$2" != "up" ]; then
 	exit 0
 fi
 
 # Do not run if we already successed:
-if [ -e "${SUCCESS_FILE}" ]; then
+if [ -e "$SUCCESS_FILE" ]; then
 	exit 0
 fi
 
 
 ### Delete previous state file
-rm -f "${DONE_FILE}"
+rm -f "$DONE_FILE"
 
 
 ### Create log file
 
 # The htp user needs to write to this file.
 # The $LIVE_USERNAME user needs to read this file.
-touch "${LOG}"
-chown htp:nogroup "${LOG}"
-chmod 644 "${LOG}"
+touch "$LOG"
+chown htp:nogroup "$LOG"
+chmod 644 "$LOG"
 
 
 ### Run tails-htp-notify-user (the sooner, the better)
@@ -88,36 +87,36 @@ chmod 644 "${LOG}"
 . /etc/live/config.d/username
 
 export DISPLAY=':0.0'
-export XAUTHORITY="`echo /var/run/gdm3/auth-for-${LIVE_USERNAME}-*/database`"
-exec /bin/su -c /usr/local/bin/tails-htp-notify-user "${LIVE_USERNAME}" &
+export XAUTHORITY="$(echo /var/run/gdm3/auth-for-$LIVE_USERNAME-*/database)"
+exec /bin/su -c /usr/local/bin/tails-htp-notify-user "$LIVE_USERNAME" &
 
 
 ### Functions
 
-log () {
+log() {
 	echo "$@" >> "${LOG}"
 }
 
-quit () {
-	exit_code="$1"
+quit() {
+	local exit_code="$1"
 	shift
-	message="$@"
+	local message="$@"
 
 	cleanup_etc_hosts
-	echo "$exit_code" >> "${DONE_FILE}"
-	if [ $exit_code -eq 0 ]; then
-		touch "${SUCCESS_FILE}"
+	echo "$exit_code" >> "$DONE_FILE"
+	if [ "$exit_code" -eq 0 ]; then
+		touch "$SUCCESS_FILE"
 	fi
-	log "${message}"
+	log "$message"
 	exit $exit_code
 }
 
 cleanup_etc_hosts() {
 	log "Cleaning /etc/hosts"
 	local tempfile
-	tempfile=`mktemp -t nm-htp.XXXXXXXX`
+	tempfile=$(mktemp -t nm-htp.XXXXXXXX)
 	where=outside
-	cat /etc/hosts | while read line ; do
+	while read line; do
 		if [ "$where" = inside ]; then
 			if [ "$line" = "$END_MAGIC" ]; then
 				where=outside
@@ -129,9 +128,20 @@ cleanup_etc_hosts() {
 				echo "$line" >> $tempfile
 			fi
 		fi
-	done
+	done < /etc/hosts
 	chmod 644 "$tempfile"
 	mv "$tempfile" /etc/hosts
+}
+
+dns_query_cmd() {
+	local host="$1"
+	local ns cmd
+
+	cmd=""
+	for ns in $NAME_SERVERS; do
+		cmd="${cmd:+$cmd || }host '$host' '$ns'"
+	done
+	echo "$cmd"
 }
 
 
@@ -139,37 +149,30 @@ cleanup_etc_hosts() {
 
 # Beware: this string is used and parsed in tails-htp-notify-user
 log "HTP NetworkManager hook: here we go"
-log "Will use these nameservers: ${NAME_SERVERS}"
+log "Will use these nameservers: $NAME_SERVERS"
 
-echo "${BEGIN_MAGIC}" >> /etc/hosts
+echo "$BEGIN_MAGIC" >> /etc/hosts
 
-for HTP_HOST in ${HTP_POOL[*]} ; do
-	DNS_QUERY_CMD=`for NS in ${NAME_SERVERS}; do
-	               echo -n "|| host ${HTP_HOST} ${NS} ";
-	               done | \
-	               tail --bytes=+4`
-	IP=$(sudo -u htp sh -c "${DNS_QUERY_CMD}" | \
-	       grep "has address" | \
-	       head -n 1 | \
-	       cut -d ' ' -f 4)
-	if [[ -z ${IP} ]]; then
-		echo "${END_MAGIC}" >> /etc/hosts
-		quit 17 "Failed to resolve ${HTP_HOST}"
-	else
-		echo "${IP}	${HTP_HOST}" >> /etc/hosts
+for HTP_HOST in $HTP_POOL; do
+	IP=$(sudo -u htp sh -c "$(dns_query_cmd "$HTP_HOST")" |
+	     awk '/ has address / { print $4 ; quit }')
+	if [ -z "$IP" ]; then
+		echo "$END_MAGIC" >> /etc/hosts
+		quit 17 "Failed to resolve $HTP_HOST"
 	fi
+	echo "$IP	$HTP_HOST" >> /etc/hosts
 done
 
-echo "${END_MAGIC}" >> /etc/hosts
+echo "$END_MAGIC" >> /etc/hosts
 
 /usr/local/sbin/htpdate \
 	-d \
-	-l "${LOG}" \
+	-l "$LOG" \
 	-a "`/usr/local/bin/getTorbuttonUserAgent`" \
 	-f \
 	-p \
 	-u htp \
-	${HTP_POOL[*]}
+	$HTP_POOL
 HTPDATE_RET=$?
 
-quit ${HTPDATE_RET} "htpdate exited with return code ${HTPDATE_RET}"
+quit $HTPDATE_RET "htpdate exited with return code $HTPDATE_RET"
