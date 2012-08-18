db08e40ae3dc8fd1610d181eb85dff6073481bca
Tails developers <amnesia@boum.org>
2012-08-18 22:30:15 +0200

    Simplify whisperback config file

--- config/chroot_local-includes/etc/whisperback/config.py
+++ config/chroot_local-includes/etc/whisperback/config.py
@@ -6,30 +6,10 @@
 # This is a python script that will be read at startup. Any python
 # syntax is valid.
 
-########################################################################
-# WhisperBack - Send a feedback in an encrypted mail
-# Copyright (C) 2009-2010 Amnesia <amnesia@boum.org>
-#
-# This file is part of WhisperBack
-#
-# WhisperBack is  free software; you can redistribute  it and/or modify
-# it under the  terms of the GNU General Public  License as published by
-# the Free Software Foundation; either  version 3 of the License, or (at
-# your option) any later version.
-# 
-# This program  is distributed in the  hope that it will  be useful, but
-# WITHOUT   ANY  WARRANTY;   without  even   the  implied   warranty  of
-# MERCHANTABILITY  or FITNESS  FOR A  PARTICULAR PURPOSE.   See  the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-########################################################################
 # IMPORTS
-# Do not change this - required to parse path
-import os.path
 
 # Custom imports
+import os
 import subprocess
 import random
 import locale
@@ -132,7 +112,7 @@ smtp_port = 25
 # The path to a file containing the certificate to trust
 # This can be either a CA certificate used to sign the SMTP server
 # certificate or the certificate of the SMTP server itself
-smtp_tlscafile = os.path.join("/", "etc", "whisperback", "4mvq3pnvid3awjln.onion.pem")
+smtp_tlscafile = "/etc/whisperback/4mvq3pnvid3awjln.onion.pem"
 
 # MESSAGE
 #
