844c6461fafdab8ea98ce006518b25943941b40e
Tails developers <amnesia@boum.org>
2012-09-01 14:58:15 +0200

    Improve languages_statistics.sh
      - move to POSIX /bin/sh
      - add vim/Emacs modelines
      - separatedly account for fuzzy translations
      - ignore obsolete strings
      - allow specifying a list of languages as command line
      - arguments
      - minor factorization
      - stricter msgid matching regexp

--- languages_statistics.sh
+++ languages_statistics.sh
@@ -1,14 +1,26 @@
-#!/bin/bash
+#!/bin/sh
+# -*- mode: sh; sh-basic-offset: 4; indent-tabs-mode: nil; -*-
+# vim: set filetype=sh sw=4 sts=4 expandtab autoindent:
 
-LANGUAGES="de es fr pt"
+set -e
 
-for LANG in $LANGUAGES ; do
-    PO_FILES="$(mktemp -t XXXXXX.$LANG)"
-    find -iname "*.$LANG.po" > $PO_FILES
-    PO_MESSAGES="$(mktemp -t XXXXXX.$LANG.po)"
+LANGUAGES=${@:-de es fr pt}
+
+count_msgids () {
+    cat | grep -E '^msgid\s+' | wc -l
+}
+
+for lang in $LANGUAGES ; do
+    PO_FILES="$(mktemp -t XXXXXX.$lang)"
+    find -iname "*.$lang.po" > $PO_FILES
+    PO_MESSAGES="$(mktemp -t XXXXXX.$lang.po)"
     msgcat --files-from=$PO_FILES --output=$PO_MESSAGES
-    TOTAL=`grep ^msgid $PO_MESSAGES | wc -l`
-    TRANSLATED=`msgattrib --translated $PO_MESSAGES | grep ^msgid | wc -l`
-    echo "$LANG: $(($TRANSLATED*100/$TOTAL))%"
+    TOTAL=$(msgattrib --no-obsolete $PO_MESSAGES | count_msgids)
+    FUZZY=$(msgattrib --only-fuzzy --no-obsolete $PO_MESSAGES | count_msgids)
+    TRANSLATED=$(
+        msgattrib --translated --no-fuzzy --no-obsolete $PO_MESSAGES \
+            | count_msgids
+    )
+    echo "$lang: $(($TRANSLATED*100/$TOTAL))% translated, $(($FUZZY*100/$TOTAL))% fuzzy"
     rm -f $PO_FILES $PO_MESSAGES
 done
