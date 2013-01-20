d9241e189ec3a219f08b3e30ac372708f8b09fc0
Tails developers <amnesia@boum.org>
2013-01-20 15:24:07 +0100

    Localize iceweasel spellchecker dictionary according to locale.
    This is achieved with our custom branding extension.

--- config/chroot_local-includes/etc/iceweasel/profile/extensions/branding@amnesia.boum.org/defaults/preferences/prefs.js
+++ config/chroot_local-includes/etc/iceweasel/profile/extensions/branding@amnesia.boum.org/defaults/preferences/prefs.js
@@ -1,3 +1,4 @@
 pref("browser.search.defaultenginename", "chrome://amnesiabranding/locale/amnesia.properties");
 pref("browser.search.selectedEngine", "chrome://amnesiabranding/locale/amnesia.properties");
 pref("browser.startup.homepage", "chrome://amnesiabranding/locale/amnesia.properties");
+pref("spellchecker.dictionary", "chrome://amnesiabranding/locale/amnesia.properties");
