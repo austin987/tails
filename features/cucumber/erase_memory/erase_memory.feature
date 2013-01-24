f7f618d0c90217866f1df994327b4db387de3d68
Tails developers <amnesia@boum.org>
2013-01-24 16:10:26 +0100

    Add feature testing memory erasure on shutdown.

---
+++ features/cucumber/erase_memory/erase_memory.feature
@@ -0,0 +1,20 @@
+Feature: System memory erasure on shutdown
+  As a Tails user
+  when I shutdown Tails
+  I want the system memory to be free from sensitive data.
+
+  Background:
+    Given a freshly started Tails with boot options "debug=wipemem"
+    And I log in to a new session
+    And GNOME has started
+    And process "memlockd" is running
+    And process "udev-watchdog" is running
+
+  Scenario: Memory must be erased on shutdown.
+    When I fill the guest's memory with a known pattern
+    And I dump the guest's memory into file "before_wipe.dump"
+    And I shutdown Tails
+    And I see "MemoryWipeCompleted.png" after at most 120 seconds
+    And I dump the guest's memory into file "after_wipe.dump"
+    Then I find at least 10000000 patterns in the dump "before_wipe.dump"
+    And I find at most 1000 patterns in the dump "after_wipe.dump"
---
+++ features/cucumber/erase_memory/step_definitions/erase_memory.rb
@@ -0,0 +1,23 @@
+Given /^I fill the guest's memory with a known pattern$/ do
+  #@vm.execute("for i in $(seq 1 16); do /usr/local/sbin/fillram & done")
+  @vm.execute("/usr/local/sbin/fillram")
+end
+
+When /^I dump the guest's memory into file "([^"]+)"$/ do |dump|
+  dump_path = "#{Dir.pwd}/features/tmpfs/#{dump}"
+  @vm.domain.core_dump(dump_path)
+end
+
+Then /^I find at least (\d+) patterns in the dump "([^"]+)"$/ do |min, dump|
+  dump_path = "#{Dir.pwd}/features/tmpfs/#{dump}"
+  hits = IO.popen("grep -c 'wipe_didnt_work' #{dump_path}").gets
+  puts "Patterns found: #{hits}"
+  assert hits.to_i >= min.to_i
+end
+
+Then /^I find at most (\d+) patterns in the dump "([^"]+)"$/ do |max, dump|
+  dump_path = "#{Dir.pwd}/features/tmpfs/#{dump}"
+  hits = IO.popen("grep -c 'wipe_didnt_work' #{dump_path}").gets
+  puts "Patterns found: #{hits}"
+  assert hits.to_i <= max.to_i
+end
