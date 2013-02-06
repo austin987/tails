588e002a31885219fe756245f19ef3123b4f5f04
Tails developers <amnesia@boum.org>
2013-02-06 16:10:55 +0100

    Add missing "hover" method to the Ruby Sikuli bindings.

--- features/cucumber/support/helpers/sikuli_helper.rb
+++ features/cucumber/support/helpers/sikuli_helper.rb
@@ -5,6 +5,18 @@ module Sikuli
   KEY_F2 = "\356\200\222"
 end
 
+module Sikuli::Clickable
+  def hover(x, y)
+    @java_obj.hover(org.sikuli.script::Location.new(x, y).offset(x(), y()))
+  end
+end
+
+class Sikuli::Screen
+  def hide_cursor()
+    hover(self.width, self.height)
+  end
+end
+
 # Configure sikuli
 Sikuli::Config.run do |config|
   config.image_path = "#{Dir.pwd}/features/cucumber/images/"
