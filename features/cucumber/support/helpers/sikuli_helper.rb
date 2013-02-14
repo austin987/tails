72eba11f2de34e16e1dbb1a369f87634bacb9504
Tails developers <amnesia@boum.org>
2013-02-14 11:43:51 +0100

    Add some more missing Sikuli key constants.

--- features/cucumber/support/helpers/sikuli_helper.rb
+++ features/cucumber/support/helpers/sikuli_helper.rb
@@ -2,7 +2,18 @@ require 'sikuli'
 
 # Add missing stuff
 module Sikuli
+  KEY_F1 = "\356\200\221"
   KEY_F2 = "\356\200\222"
+  KEY_F3 = "\356\200\223"
+  KEY_F4 = "\356\200\224"
+  KEY_F5 = "\356\200\225"
+  KEY_F6 = "\356\200\226"
+  KEY_F7 = "\356\200\227"
+  KEY_F8 = "\356\200\228"
+  KEY_F9 = "\356\200\229"
+  KEY_F10 = "\356\200\230"
+  KEY_F11 = "\356\200\231"
+  KEY_F12 = "\356\200\232"
 end
 
 module Sikuli::Clickable
