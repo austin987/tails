6afe6479d502b49905b0e1d45fb4db95694f922f
Tails developers <amnesia@boum.org>
2013-02-20 15:23:04 +0100

    Optionally set message for failed assertions or try_for calls.

--- features/cucumber/support/helpers/misc_helpers.rb
+++ features/cucumber/support/helpers/misc_helpers.rb
@@ -1,22 +1,30 @@
 require 'date'
 require 'system_timer'
 
-def assert(b)
-  raise RuntimeError, "Assertion failed!", caller if ! b
+def assert(b, msg = "Assertion failed!")
+  raise RuntimeError, msg, caller if ! b
 end
 
 # Call block (ignoring any exceptions it may throw) repeatedly with one
 # second breaks until it returns true, or until `t` seconds have
 # passed when we throw Timeout:Error.
-def try_for(t, delay = 1)
-  SystemTimer.timeout(t) do
-    loop do
-      begin
-        return true if yield
-      rescue Exception
-        # noop
+def try_for(t, delay = 1, msg = nil)
+  begin
+    SystemTimer.timeout(t) do
+      loop do
+        begin
+          return true if yield
+        rescue Exception
+          # noop
+        end
+        sleep delay
       end
-      sleep delay
+    end
+  rescue Timeout::Error => e
+    if msg
+      raise RuntimeError, msg, caller
+    else
+      raise e
     end
   end
   return false
