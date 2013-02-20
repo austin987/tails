7cbf3588ce96a0326c99fd424d72956b42c2f214
Tails developers <amnesia@boum.org>
2013-02-20 15:49:41 +0100

    Add hook that runs at start of each feature.

--- features/cucumber/support/env.rb
+++ features/cucumber/support/env.rb
@@ -6,10 +6,18 @@ Before do |scenario|
   @screen = Sikuli::Screen.new
   @sniffer = Sniffer.new("TestSniffer", @vm.net.bridge_name, @vm.ip, @vm.ip6)
   @sniffer.capture
-  @feature = File.basename(scenario.feature.file, ".feature")
+  @feature = File.basename(scenario.feature.file, ".feature").to_s
   @background_snapshot = "#{Dir.pwd}/features/tmpfs/#{@feature}_background.state"
   @skip_steps_while_restoring_background = false
   @theme = "gnome"
+  if $prev_feature != @feature
+    # This code runs before the *first* scenario's background for each feature
+    # FIXME: We can't use this yet, due to permission issues.
+#    if File.exist? @background_snapshot
+#      File.delete @background_snapshot
+#    end
+  end
+  $prev_feature = @feature
 end
 
 
