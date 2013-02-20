9f3259946abfad0d2ddb05c64db46eee3ef3c6c2
Tails developers <amnesia@boum.org>
2013-02-20 15:34:31 +0100

    Use constant names from java instead of magic numbers.

--- features/cucumber/support/helpers/sikuli_helper.rb
+++ features/cucumber/support/helpers/sikuli_helper.rb
@@ -2,18 +2,18 @@ require 'sikuli'
 
 # Add missing stuff
 module Sikuli
-  KEY_F1 = "\356\200\221"
-  KEY_F2 = "\356\200\222"
-  KEY_F3 = "\356\200\223"
-  KEY_F4 = "\356\200\224"
-  KEY_F5 = "\356\200\225"
-  KEY_F6 = "\356\200\226"
-  KEY_F7 = "\356\200\227"
-  KEY_F8 = "\356\200\228"
-  KEY_F9 = "\356\200\229"
-  KEY_F10 = "\356\200\230"
-  KEY_F11 = "\356\200\231"
-  KEY_F12 = "\356\200\232"
+  KEY_F1 = Key::F1
+  KEY_F2 = Key::F2
+  KEY_F3 = Key::F3
+  KEY_F4 = Key::F4
+  KEY_F5 = Key::F5
+  KEY_F6 = Key::F6
+  KEY_F7 = Key::F7
+  KEY_F8 = Key::F8
+  KEY_F9 = Key::F9
+  KEY_F10 = Key::F10
+  KEY_F11 = Key::F11
+  KEY_F12 = Key::F12
   KEY_ESC = Key::ESC
 end
 
