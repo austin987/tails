ce3d9bb9c8bc037faa3b27a8b0294d211e607fb7
Tails developers <amnesia@boum.org>
2013-02-22 15:27:42 +0100

    Print time of scenario failure.
    This is pretty useful in combination with `run_test_suite --capture`,
    so one can quickly jump to the time of failure in the video.

--- features/cucumber/support/env.rb
+++ features/cucumber/support/env.rb
@@ -1,14 +1,20 @@
 require 'java'
 require 'rubygems'
+require 'time'
 
 Before do |scenario|
+  if ! $already_run
+    # This code runs *exactly once*, before the *furst* feature
+    $time_at_start = Time.now
+    $already_run = true
+  end
   @screen = Sikuli::Screen.new
   @feature = File.basename(scenario.feature.file, ".feature").to_s
   @background_snapshot = "#{Dir.pwd}/features/tmpfs/#{@feature}_background.state"
   @skip_steps_while_restoring_background = false
   @theme = "gnome"
   if $prev_feature != @feature
-    # This code runs before the *first* scenario's background for each feature
+    # This code runs before the *first* scenario's background for *each* feature
     # FIXME: We can't use this yet, due to permission issues.
 #    if File.exist? @background_snapshot
 #      File.delete @background_snapshot
@@ -20,7 +26,12 @@ end
 
 After do |scenario|
   if (scenario.status != :passed)
-    @vm.take_screenshot("#{@feature}-#{DateTime.now}")
+    time_of_fail = Time.now - $time_at_start
+    secs = "%02d" % (time_of_fail % 60)
+    mins = "%02d" % ((time_of_fail / 60) % 60)
+    hrs  = "%02d" % (time_of_fail / (60*60))
+    STDERR.puts "Scenario failed at time #{hrs}:#{mins}:#{secs}"
+    @vm.take_screenshot("#{@feature}-#{DateTime.now}") if @vm
   end
   @sniffer.stop if @sniffer
   @vm.destroy
