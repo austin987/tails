d66ff88368ef202e072eddb847e7108d95a7d9f8
Tails developers <amnesia@boum.org>
2013-03-08 01:26:35 +0100

    Add a helpful error message.

--- features/cucumber/support/helpers/display_helper.rb
+++ features/cucumber/support/helpers/display_helper.rb
@@ -11,7 +11,9 @@ class Display
     # We wait for the display to be active to not lose actions
     # (e.g. key presses via sikuli) that come immediately after
     # starting (or restoring) a vm
-    try_for(20, :delay => 0.1) { active? }
+    try_for(20, { :delay => 0.1, :msg => "virt-viewer failed to start"}) {
+      active?
+    }
   end
 
   def stop
