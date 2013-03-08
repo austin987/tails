486f45ed95cd872c6afc80bd5fb1e62911ee4f2d
Tails developers <amnesia@boum.org>
2013-03-08 01:08:40 +0100

    Move code from wrapper to cucumber environment.
    It really belongs there, and helps when not using the wrapper.

--- features/cucumber/support/env.rb
+++ features/cucumber/support/env.rb
@@ -1,12 +1,53 @@
 require 'java'
 require 'rubygems'
 require 'time'
+require 'fileutils'
 require "#{Dir.pwd}/features/cucumber/support/extra_hooks.rb"
 
 $time_at_start = Time.now
-$tmp_dir = ENV['TEMP_DIR']
+$tmp_dir = ENV['TEMP_DIR'] || "/tmp/TailsToaster"
+if File.exist?($tmp_dir)
+  if !File.directory?($tmp_dir)
+    raise "Temporary directory '#{$tmp_dir}' exists but is not a " +
+          "directory"
+  end
+  if !File.owned?($tmp_dir)
+    raise "Temporary directory '#{$tmp_dir}' must be owned by the " +
+          "current user"
+  end
+  FileUtils.chmod(0755, $tmp_dir)
+else
+  begin
+    Dir.mkdir($tmp_dir)
+  rescue Errno::EACCES => e
+    raise "Cannot create temporary directory: #{e.to_s}"
+  end
+end
 $vm_xml_path = ENV['VM_XML_PATH'] || "#{Dir.pwd}/features/cucumber/domains"
 $tails_iso = ENV['ISO'] || get_last_iso
+if $tails_iso.nil?
+  raise "No Tails ISO image specified, and none could be found in the " +
+        "current directory"
+end
+if File.exist?($tails_iso)
+  # Workaround: when libvirt takes ownership of the ISO image it may
+  # become unreadable for the amnesia user inside the guest in the
+  # host-to-guest share used for some tests.
+
+  # jruby 1.5.6 doesn't have world_readable? in File or File::Stat so we
+  # manually check for it in the mode string
+  if !(File.stat($tails_iso).mode & 04)
+    if File.owned?($tails_iso)
+      chmod(0644, $tails_iso)
+    else
+      raise "warning: the Tails ISO image must be world readable or be owned" +
+            "by the current user to be available inside the guest VM via " +
+            "host-to-guest shares, which is required by some tests"
+    end
+  end
+else
+  raise "The specified Tails ISO image '#{$tails_iso}' does not exist"
+end
 $x_display = ENV['DISPLAY']
 
 BeforeFeature do |feature|
