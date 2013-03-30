4c3f9023036dfa87903227425fb7bc7501ece715
Tails developers <amnesia@boum.org>
2013-03-30 11:50:11 +0100

    Don't capture ARP and RARP packets.
    Tails does not pretend restricting ARP traffic in any way.

--- features/support/helpers/net_helper.rb
+++ features/support/helpers/net_helper.rb
@@ -23,8 +23,8 @@ class Sniffer
 
   # FIXME: Do we also want to keep "ether dst host #{@mac}"? We should if we
   # want to test the firewall's INPUT dropping.
-  def capture(filter="ether src host #{@mac}")
-    job = IO.popen("/usr/sbin/tcpdump -n -i #{@bridge_name} -w #{@pcap_file} -U #{filter} >/dev/null 2>&1")
+  def capture(filter="ether src host #{@mac} and not ether proto \\arp and not ether proto \\rarp")
+    job = IO.popen("/usr/sbin/tcpdump -n -i #{@bridge_name} -w #{@pcap_file} -U '#{filter}' >/dev/null 2>&1")
     @pid = job.pid
   end
 
