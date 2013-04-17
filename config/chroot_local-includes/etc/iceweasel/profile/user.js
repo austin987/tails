ea0e00dfeed133eb27be36b89a904046a12b9fe1
Tails developers <amnesia@boum.org>
2013-04-17 21:46:22 +0200

    Set network.proxy.socks_version consistently with similar prefs.
    Else we get the meaningless 'user_pref("network.proxy.socks_version", 9063);' in
    prefs.js after the initial startup.

--- config/chroot_local-includes/etc/iceweasel/profile/user.js
+++ config/chroot_local-includes/etc/iceweasel/profile/user.js
@@ -59,6 +59,7 @@ user_pref("extensions.torbutton.saved.type", 1);
 user_pref("network.proxy.socks", "127.0.0.1");
 user_pref("network.proxy.socks_port", 9063);
 user_pref("network.proxy.socks_remote_dns", true);
+user_pref("network.proxy.socks_version", 5);
 user_pref("network.proxy.type", 1);
 
 // Other non-Torbutton prefs
