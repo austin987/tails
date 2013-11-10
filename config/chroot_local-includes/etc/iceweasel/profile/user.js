4f758c12b99e3a5bc2eace3aaea0fdb8d24a37b9
Tails developers <amnesia@boum.org>
2013-11-10 12:14:31 +0000

    Drop useless browser.cache.disk.capacity Iceweasel pref.
    It's ignored unless browser.cache.disk.enable is enabled,
    and we have it set to false.

--- config/chroot_local-includes/etc/iceweasel/profile/user.js
+++ config/chroot_local-includes/etc/iceweasel/profile/user.js
@@ -60,7 +60,6 @@ user_pref("extensions.torbutton.saved.type", 1);
 user_pref("network.proxy.socks_port", 9151);
 
 // Other non-Torbutton prefs
-user_pref("browser.cache.disk.capacity", 0);
 user_pref("browser.chrome.favicons", false);
 user_pref("browser.chrome.image_icons.max_size", 0);
 user_pref("browser.download.manager.closeWhenDone", true);
