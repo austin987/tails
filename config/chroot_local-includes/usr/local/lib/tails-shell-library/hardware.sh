df92495e790042cb6648b2deb56b4beb36bd0296
Tails developers <amnesia@boum.org>
2013-11-25 14:58:47 +0100

    Skip trying (and failing) to spoof non-existing NICs.

--- config/chroot_local-includes/usr/local/lib/tails-shell-library/hardware.sh
+++ config/chroot_local-includes/usr/local/lib/tails-shell-library/hardware.sh
@@ -4,6 +4,10 @@ get_all_ethernet_nics() {
     /sbin/ifconfig -a | grep "Link encap:Ethernet" | cut -f 1 -d " "
 }
 
+nic_exists() {
+    /sbin/ifconfig "${1}" >/dev/null 2>&1
+}
+
 nic_is_up() {
     /sbin/ifconfig | grep -qe "^${1}\>"
 }
