7efb0b5ec047db61b182d25d53d2876b40aea281
Tails developers <amnesia@boum.org>
2014-01-07 15:08:59 +0000

    Gettext-ize the two new scripts brought by feature/dont_autostart_iceweasel.

--- config/chroot_local-includes/etc/NetworkManager/dispatcher.d/60-tor-ready-notification.sh
+++ config/chroot_local-includes/etc/NetworkManager/dispatcher.d/60-tor-ready-notification.sh
@@ -10,6 +10,11 @@ if [ $2 != "up" ]; then
    exit 0
 fi
 
-#XXX gettextize
-/usr/local/sbin/tails-notify-user "Tor is now ready" \
-   "Tor is ready. You can now use the Internet."
+# Initialize gettext support
+. gettext.sh
+TEXTDOMAIN="tails"
+export TEXTDOMAIN
+
+/usr/local/sbin/tails-notify-user \
+   "`gettext \"Tor is now ready\"`" \
+   "`gettext \"Tor is ready. You can now use the Internet.\"`"
