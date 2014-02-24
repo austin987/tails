241f32982cee29b1872c81fd0d1049da4610154b
Tails developers <amnesia@boum.org>
2014-02-24 15:53:28 +0100

    Add shell library functions for modifying torrc.

--- config/chroot_local-includes/usr/local/lib/tails-shell-library/tor.sh
+++ config/chroot_local-includes/usr/local/lib/tails-shell-library/tor.sh
@@ -46,3 +46,15 @@ tor_bootstrap_progress() {
 tor_is_working() {
 	[ -e $TOR_DESCRIPTORS ] || [ -e $NEW_TOR_DESCRIPTORS ]
 }
+
+tor_append_to_torrc () {
+	echo "${@}" >> "${TOR_RC}"
+}
+
+# Set a (possibly existing) option $1 to $2 in torrc. Shouldn't be
+# used for options that can be set multiple times (e.g. the listener
+# options).
+tor_set_in_torrc () {
+	sed -i "/^${1}\s/d" "${TOR_RC}"
+	tor_append_to_torrc "${1} ${2}"
+}
