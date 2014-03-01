4d5f094e88ff749c92d1a005ee4b8824341406db
Tails developers <amnesia@boum.org>
2014-03-01 13:59:00 +0000

    Explicitly tell virsh which libvirt URI it needs to connect to.
    Else, when the test suite is not run as root, virsh connects to the *user*
    libvirt instance by default, instead of the system one, and as a result it
    cannot even see the TailsToaster domain running, as noticed by anonym:
    https://labs.riseup.net/code/issues/5959#note-9

--- features/support/helpers/vm_helper.rb
+++ features/support/helpers/vm_helper.rb
@@ -363,7 +363,7 @@ EOF
   def reset
     # ruby-libvirt 0.4 does not support the reset method.
     # XXX: Once we use Jessie, use @domain.reset instead.
-    system("virsh reset " + @domain_name) if is_running?
+    system("virsh -c qemu:///system reset " + @domain_name) if is_running?
   end
 
   def power_off
