3efd9db9c1327fc5bd41907f8dd2cf444e0519be
Tails developers <amnesia@boum.org>
2014-03-05 01:04:35 +0000

    Document unsupported stuff.

--- config/chroot_local-includes/usr/local/lib/tails-shell-library/tor.sh
+++ config/chroot_local-includes/usr/local/lib/tails-shell-library/tor.sh
@@ -53,7 +53,8 @@ tor_append_to_torrc () {
 
 # Set a (possibly existing) option $1 to $2 in torrc. Shouldn't be
 # used for options that can be set multiple times (e.g. the listener
-# options).
+# options). Does not support configuration entries split into multiple
+# lines (with the backslash character).
 tor_set_in_torrc () {
 	sed -i "/^${1}\s/d" "${TOR_RC}"
 	tor_append_to_torrc "${1} ${2}"
