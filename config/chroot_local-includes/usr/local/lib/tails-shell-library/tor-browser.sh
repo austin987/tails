8cd798344fc7487516ad63cbcc00773cf20ebdf3
Tails developers <amnesia@boum.org>
2014-10-02 14:18:44 +0200

    Refactor TBB firefox start into exec_firefox() library function.
    That way one only has to remember to always call firefox with
    exec_firefox(), and not to set LD_LIBRARY_PATH. As an example, this
    was forgotten for Tor Launcher, which also was fixed.

--- config/chroot_local-includes/usr/local/lib/tails-shell-library/tor-browser.sh
+++ config/chroot_local-includes/usr/local/lib/tails-shell-library/tor-browser.sh
@@ -3,3 +3,9 @@
 TBB_INSTALL=/usr/local/lib/tor-browser
 TBB_PROFILE=/etc/tor-browser/profile
 TBB_EXT="${TBB_INSTALL}/extensions"
+
+exec_firefox() {
+    LD_LIBRARY_PATH="${TBB_INSTALL}"/Browser
+    export LD_LIBRARY_PATH
+    exec "${TBB_INSTALL}"/Browser/firefox "${@}"
+}
