a6c9aab64f3c0263313878f2b00b0813f96c0208
Tails developers <amnesia@boum.org>
2014-10-10 12:39:27 +0200

    Move TBB's global extensions dir.
    The new place corresponds better to where Debian puts the Iceweasel
    extensions. Also, it will allow us to drop the useless "Browser"
    sub-dir in TBB_INSTALL without having potential collisions (e.g. if
    there was a "Browser/extensions").

--- config/chroot_local-includes/usr/local/lib/tails-shell-library/tor-browser.sh
+++ config/chroot_local-includes/usr/local/lib/tails-shell-library/tor-browser.sh
@@ -2,7 +2,7 @@
 
 TBB_INSTALL=/usr/local/lib/tor-browser
 TBB_PROFILE=/etc/tor-browser/profile
-TBB_EXT="${TBB_INSTALL}/extensions"
+TBB_EXT=/usr/local/share/tor-browser-extensions
 
 exec_firefox() {
     LD_LIBRARY_PATH="${TBB_INSTALL}"/Browser
