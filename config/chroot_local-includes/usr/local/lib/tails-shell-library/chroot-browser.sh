eb9d3d4c3b5803ab86bb12da7eed823a406514f9
Tails developers <amnesia@boum.org>
2014-10-20 23:33:09 +0200

    Refactor chroot browsers' setup_chroot().

--- config/chroot_local-includes/usr/local/lib/tails-shell-library/chroot-browser.sh
+++ config/chroot_local-includes/usr/local/lib/tails-shell-library/chroot-browser.sh
@@ -28,3 +28,39 @@ try_cleanup_browser_chroot () {
     done
     rmdir ${cow} ${chroot} 2>/dev/null
 }
+
+# Setup a chroot on a clean aufs "fork" of the root filesystem.
+setup_browser_chroot () {
+    local chroot="${1}"
+    local cow="${2}"
+    # FIXME: When LXC matures to the point where it becomes a viable option
+    # for creating isolated jails, the chroot can be used as its rootfs.
+
+    trap cleanup INT
+    trap cleanup EXIT
+
+    local rootfs_dir
+    local rootfs_dirs_path=/lib/live/mount/rootfs
+    local tails_module_path=/lib/live/mount/medium/live/Tails.module
+    local aufs_dirs=
+
+    # We have to pay attention to the order we stack the filesystems;
+    # newest must be first, and remember that the .module file lists
+    # oldest first, newest last.
+    while read rootfs_dir; do
+        rootfs_dir="${rootfs_dirs_path}/${rootfs_dir}"
+        mountpoint -q "${rootfs_dir}" && \
+        aufs_dirs="${rootfs_dir}=rr+wh:${aufs_dirs}"
+    done < "${tails_module_path}"
+    # But our copy-on-write dir must be at the very top.
+    aufs_dirs="${cow}=rw:${aufs_dirs}"
+
+    mkdir -p ${cow} ${chroot} && \
+    mount -t tmpfs tmpfs ${cow} && \
+    mount -t aufs -o "noatime,noxino,dirs=${aufs_dirs}" aufs ${chroot} && \
+    mount -t proc proc ${chroot}/proc && \
+    mount --bind /dev ${chroot}/dev
+
+    # Workaround for #6110
+    chmod -t ${cow}
+}
