0bb5e6bb7b362b24b2c1efe037fcb79d476442bb
Tails developers <amnesia@boum.org>
2014-10-21 18:58:56 +0200

    Make language code extraction more robust.
    There are three letter language codes, e.g. fur.

--- config/chroot_local-includes/usr/local/lib/tails-shell-library/localization.sh
+++ config/chroot_local-includes/usr/local/lib/tails-shell-library/localization.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+language_code_from_locale () {
+    echo "${1}" | sed "s,\(_\|\.\).*$,,"
+}
+
 localize_tails_doc_page () {
     local page="${1}"
-    local lang_code="$(echo ${LANG} | head -c 2)"
+    local lang_code="$(language_code_from_locale "${LANG}")"
     local try_page
     for locale in "${lang_code}" "en"; do
         try_page="${page}.${locale}.html"
