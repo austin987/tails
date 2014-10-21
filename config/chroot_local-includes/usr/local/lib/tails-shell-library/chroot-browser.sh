a6043ab74108f88207e6af20cc1918266d937a9e
Tails developers <amnesia@boum.org>
2014-10-21 13:45:50 +0200

    Use quoting on variable and assignment values more consistently.

--- config/chroot_local-includes/usr/local/lib/tails-shell-library/chroot-browser.sh
+++ config/chroot_local-includes/usr/local/lib/tails-shell-library/chroot-browser.sh
@@ -16,11 +16,11 @@ try_cleanup_browser_chroot () {
     local cow="${2}"
     local user="${3}"
     try_for 10 "pkill -u ${user} 1>/dev/null 2>&1" 0.1 || \
-        pkill -9 -u ${user} 1>/dev/null 2>&1
-    for mnt in ${chroot}/dev ${chroot}/proc ${chroot} ${cow}; do
+        pkill -9 -u "${user}" 1>/dev/null 2>&1
+    for mnt in "${chroot}"/dev "${chroot}"/proc "${chroot}" "${cow}"; do
         try_for 10 "umount ${mnt} 2>/dev/null" 0.1
     done
-    rmdir ${cow} ${chroot} 2>/dev/null
+    rmdir "${cow}" "${chroot}" 2>/dev/null
 }
 
 # Setup a chroot on a clean aufs "fork" of the root filesystem.
@@ -34,8 +34,8 @@ setup_browser_chroot () {
     trap cleanup EXIT
 
     local rootfs_dir
-    local rootfs_dirs_path=/lib/live/mount/rootfs
-    local tails_module_path=/lib/live/mount/medium/live/Tails.module
+    local rootfs_dirs_path="/lib/live/mount/rootfs"
+    local tails_module_path="/lib/live/mount/medium/live/Tails.module"
     local aufs_dirs=
 
     # We have to pay attention to the order we stack the filesystems;
@@ -49,15 +49,15 @@ setup_browser_chroot () {
     # But our copy-on-write dir must be at the very top.
     aufs_dirs="${cow}=rw:${aufs_dirs}"
 
-    mkdir -p ${cow} ${chroot} && \
-    mount -t tmpfs tmpfs ${cow} && \
-    mount -t aufs -o "noatime,noxino,dirs=${aufs_dirs}" aufs ${chroot} && \
-    mount -t proc proc ${chroot}/proc && \
-    mount --bind /dev ${chroot}/dev || \
+    mkdir -p "${cow}" "${chroot}" && \
+    mount -t tmpfs tmpfs "${cow}" && \
+    mount -t aufs -o "noatime,noxino,dirs=${aufs_dirs}" aufs "${chroot}" && \
+    mount -t proc proc "${chroot}"/proc && \
+    mount --bind /dev "${chroot}"/dev || \
         return 1
 
     # Workaround for #6110
-    chmod -t ${cow}
+    chmod -t "${cow}"
 }
 
 set_chroot_browser_locale () {
@@ -73,18 +73,18 @@ set_chroot_browser_name () {
     local chroot="${1}"
     local name="${2}"
     local locale="${3}"
-    local ext_dir=${chroot}/"${TBB_EXT}"
+    local ext_dir="${chroot}/${TBB_EXT}"
     local pack top rest
-    if [ "${locale}" != en-US ]; then
+    if [ "${locale}" != "en-US" ]; then
         pack="${ext_dir}/langpack-${locale}@firefox.mozilla.org.xpi"
-        top=browser/chrome
-        rest=${locale}/locale
+        top="browser/chrome"
+        rest="${locale}/locale"
     else
         pack="${chroot}/${TBB_INSTALL}/browser/omni.ja"
-        top=chrome
-        rest=en-US/locale
+        top="chrome"
+        rest="en-US/locale"
     fi
-    local tmp=$(mktemp -d)
+    local tmp="$(mktemp -d)"
     local branding="${top}/${rest}/branding/brand.dtd"
     # Non-zero exit code due to non-standard ZIP archive.
     # The following steps will fail soon if the extraction failed anyway.
@@ -131,7 +131,7 @@ configure_chroot_dns_servers () {
 
     rm -f ${chroot}/etc/resolv.conf
     for ns in ${ip4_nameservers}; do
-        echo "nameserver ${ns}" >> ${chroot}/etc/resolv.conf
+        echo "nameserver ${ns}" >> "${chroot}/etc/resolv.conf"
     done
     chmod a+r ${chroot}/etc/resolv.conf
 }
@@ -141,7 +141,7 @@ set_chroot_browser_permissions () {
     local browser_name="${2}"
     local browser_user="${3}"
     local browser_conf="${chroot}/home/${browser_user}/.${browser_name}"
-    chown -R ${browser_user}:${browser_user} "${browser_conf}"
+    chown -R "${browser_user}:${browser_user}" "${browser_conf}"
 }
 
 configure_chroot_browser () {
@@ -153,12 +153,12 @@ configure_chroot_browser () {
     # to extensions to enable.
 
     # Prevent sudo from complaining about failing to resolve the 'amnesia' host
-    echo "127.0.0.1 localhost amnesia" > ${chroot}/etc/hosts
+    echo "127.0.0.1 localhost amnesia" > "${chroot}/etc/hosts"
 
     # Create a fresh browser profile for the clearnet user
     local browser_conf="${chroot}/home/${browser_user}/.${browser_name}"
     local browser_profile="${browser_conf}/profile.default"
-    local browser_ext="${browser_profile}"/extensions
+    local browser_ext="${browser_profile}/extensions"
     mkdir -p "${browser_profile}" "${browser_ext}"
 
     # Select extensions to enable
@@ -169,23 +169,23 @@ configure_chroot_browser () {
     done
 
     # Set preferences
-    local browser_prefs="${browser_profile}"/preferences/prefs.js
+    local browser_prefs="${browser_profile}/preferences/prefs.js"
     mkdir -p "$(dirname "${browser_prefs}")"
-    cp /usr/share/tails/"${browser_name}"/prefs.js "${browser_prefs}"
+    cp /usr/share/tails/"${browser_name}/prefs.js" "${browser_prefs}"
 
     # Set start page to something that explains what's going on
-    echo 'user_pref("browser.startup.homepage", "'${startpage}'");' >> \
+    echo 'user_pref("browser.startup.homepage", "'"${startpage}"'");' >> \
         "${browser_prefs}"
 
     # Customize the GUI
     local browser_chrome="${browser_profile}/chrome/userChrome.css"
     mkdir -p "$(dirname "${browser_chrome}")"
-    cp /usr/share/tails/"${browser_name}"/userChrome.css ${browser_chrome}
+    cp /usr/share/tails/"${browser_name}"/userChrome.css "${browser_chrome}"
 
     # Remove all bookmarks
-    rm -f ${chroot}/"${TBB_PROFILE}"/bookmarks.html
-    rm -f ${browser_profile}/bookmarks.html
-    rm -f ${browser_profile}/places.sqlite
+    rm -f "${chroot}/${TBB_PROFILE}/bookmarks.html"
+    rm -f "${browser_profile}/bookmarks.html"
+    rm -f "${browser_profile}/places.sqlite"
 
     # Set an appropriate theme, except if we're using Windows
     # camouflage.
@@ -194,9 +194,9 @@ configure_chroot_browser () {
     else
         # The tails-activate-win8-theme script requires that the
         # browser profile is writable by the user running the script.
-        set_chroot_browser_permissions ${chroot} ${browser_user}
+        set_chroot_browser_permissions "${chroot}" "${browser_user}"
         # The camouflage activation script requires a dbus server for
         # properly configuring GNOME, so we start one in the chroot
-        chroot ${chroot} sudo -H -u ${browser_user} sh -c 'eval `dbus-launch --auto-syntax`; tails-activate-win8-theme' || :
+        chroot "${chroot}" sudo -H -u "${browser_user}" sh -c 'eval `dbus-launch --auto-syntax`; tails-activate-win8-theme' || :
     fi
 }
