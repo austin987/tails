c8645facb15c5f412765b20fcca6af167cb0d80e
Tails developers <amnesia@boum.org>
2014-11-27 20:14:41 +0000

    Fix the time sync notification, in the same way as tails-notify-user was fixed already.

--- config/chroot_local-includes/etc/NetworkManager/dispatcher.d/20-time.sh
+++ config/chroot_local-includes/etc/NetworkManager/dispatcher.d/20-time.sh
@@ -213,6 +213,9 @@ is_clock_way_off() {
 start_notification_helper() {
 	export DISPLAY=':0.0'
 	export XAUTHORITY="$(echo /var/run/gdm3/auth-for-$LIVE_USERNAME-*/database)"
+	GNOME_SHELL_PID="$(pgrep --newest --euid ${LIVE_USERNAME} gnome-shell)"
+	export "$(tr '\0' '\n' < /proc/${GNOME_SHELL_PID}/environ | \
+		grep '^DBUS_SESSION_BUS_ADDRESS=')"
 	exec /bin/su -c /usr/local/bin/tails-htp-notify-user "$LIVE_USERNAME" &
 }
 
