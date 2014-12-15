86ce612e3b1dfdabfef213835debf1c6f956b479
Tails developers <amnesia@boum.org>
2014-12-15 21:54:14 +0100

    Document some more library functions.

--- config/chroot_local-includes/usr/local/lib/tails-shell-library/localization.sh
+++ config/chroot_local-includes/usr/local/lib/tails-shell-library/localization.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
+# Extracts the language part of a given locale, e.g. "en_US.UTF-8"
+# yields "en". Often $LANG will be passed as the argument.
 language_code_from_locale () {
     echo "${1}" | sed "s,\(_\|\.\).*$,,"
 }
 
+# Prints the path to the localized (according to the environment's
+# LANG) version of `page` in the local copy of Tails' website. `page`
+# should specify only the name of the page, not the language code (of
+# course!) or the ".html" extension. If a localized page doesn't exist
+# the default is the English version.
 localized_tails_doc_page () {
     local page="${1}"
     local lang_code="$(language_code_from_locale "${LANG}")"
