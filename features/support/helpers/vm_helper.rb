24512cd201f7158aa00ba2b53c452ece0837e429
Tails developers <tails@boum.org>
2015-01-11 09:14:19 -0500

    Add helper function to check if a directory exists.

--- features/support/helpers/vm_helper.rb
+++ features/support/helpers/vm_helper.rb
@@ -365,6 +365,10 @@ EOF
     execute("test -e #{file}").success?
   end
 
+  def dir_exist?(dir)
+    execute("test -d #{dir}").success?
+  end
+
   def file_content(file, user = 'root')
     # We don't quote #{file} on purpose: we sometimes pass environment variables
     # or globs that we want to be interpreted by the shell.
