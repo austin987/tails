26e571174d3240372a34b777e00fe2337cfede5f
Tails developers <amnesia@boum.org>
2015-01-16 13:50:10 +0100

    Do not auto-pick ISO images to test.
    This "feature" may give unexpected results, so it's better to force
    the user to be explicit about which ISO images to test. Furthermore,
    it introduces complexity, and will require special casing for the plan
    of improving the test suite's configuration.

--- features/support/config.rb
+++ features/support/config.rb
@@ -2,8 +2,8 @@ require 'fileutils'
 require "#{Dir.pwd}/features/support/helpers/misc_helpers.rb"
 
 # Dynamic
-$tails_iso = ENV['ISO'] || get_newest_iso
-$old_tails_iso = ENV['OLD_ISO'] || get_oldest_iso
+$tails_iso = ENV['ISO'] || raise "No ISO set with --iso"
+$old_tails_iso = ENV['OLD_ISO'] || raise "No old ISO set with --old-iso"
 $tmp_dir = ENV['TEMP_DIR'] || "/tmp/TailsToaster"
 $vm_xml_path = ENV['VM_XML_PATH'] || "#{Dir.pwd}/features/domains"
 $misc_files_dir = "#{Dir.pwd}/features/misc_files"
