ead90b15f07c9692c9a7e6ab9ef4111b3a22937d
Tails developers <amnesia@boum.org>
2015-01-26 20:13:02 +0100

    Verify that the time set by `date` actually is set (Will-fix: #5836).

--- features/step_definitions/time_syncing.rb
+++ features/step_definitions/time_syncing.rb
@@ -1,11 +1,29 @@
 When /^I set the system time to "([^"]+)"$/ do |time|
   next if @skip_steps_while_restoring_background
   @vm.execute_successfully("date -s '#{time}'")
+  new_time = DateTime.parse(@vm.execute_successfully("date").stdout).to_time
+  # Allow 1 seconds delays.
+  expected_time_lower_bound = DateTime.parse(time).to_time
+  expected_time_upper_bound = expected_time_lower_bound + 1
+  assert(expected_time_lower_bound >= new_time &&
+                    new_time <= expected_time_upper_bound,
+         "The guest's time was suppsed to be set to " \
+         "'#{expected_time_lower_bound}' but is '#{new_time}'")
 end
 
 When /^I bump the system time with "([^"]+)"$/ do |timediff|
   next if @skip_steps_while_restoring_background
+  old_time = DateTime.parse(@vm.execute_successfully("date").stdout).to_time
   @vm.execute_successfully("date -s 'now #{timediff}'")
+  new_time = DateTime.parse(@vm.execute_successfully("date").stdout).to_time
+  # Allow 1 seconds delays.
+  expected_time_lower_bound = DateTime.parse(
+      cmd_helper("date -d '#{old_time} #{timediff}'")).to_time
+  expected_time_upper_bound = expected_time_lower_bound + 1
+  assert(expected_time_lower_bound >= new_time &&
+                    new_time <= expected_time_upper_bound,
+         "The guest's time was suppsed to be bumped to " \
+         "'#{expected_time_lower_bound}' but is '#{new_time}'")
 end
 
 Then /^Tails clock is less than (\d+) minutes incorrect$/ do |max_diff_mins|
