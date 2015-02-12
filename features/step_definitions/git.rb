bde3742910b92df23e7fd5467648e7dc001037cb
Tails developers <amnesia@boum.org>
2015-02-12 12:47:45 +0000

    Use install(1) instead of mkdir + chmod.

--- features/step_definitions/git.rb
+++ features/step_definitions/git.rb
@@ -7,14 +7,13 @@ end
 
 Given /^I have the SSH key for a Git repository$/ do
   next if @skip_steps_while_restoring_background
-  @vm.execute_successfully("mkdir -p /home/#{$live_user}/.ssh/", $live_user)
+  @vm.execute_successfully("install -m 0700 -d /home/#{$live_user}/.ssh/", $live_user)
   secret_key=ENV['TAILS_TEST_SECRET_KEY']
   public_key=ENV['TAILS_TEST_PUBLIC_KEY']
   assert(!secret_key.nil? && secret_key.length > 0)
   assert(!public_key.nil? && public_key.length > 0)
   @vm.execute_successfully("echo '#{secret_key}' > /home/#{$live_user}/.ssh/id_rsa", $live_user)
   @vm.execute_successfully("echo '#{public_key}' > /home/#{$live_user}/.ssh/id_rsa.pub", $live_user)
-  @vm.execute_successfully("chmod 700 /home/#{$live_user}/.ssh/", $live_user)
   @vm.execute_successfully("chmod 600 /home/#{$live_user}/.ssh/id*", $live_user)
 end
 
