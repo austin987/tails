464d086f344181bdc155864b689f655a7b1704ff
Tails developers <amnesia@boum.org>
2015-02-12 12:31:58 +0000

    Add missing shell argument quoting.

--- features/step_definitions/git.rb
+++ features/step_definitions/git.rb
@@ -2,5 +2,5 @@ Then /^the Git repository "([\S]+)" has been cloned successfully$/ do |repo|
   next if @skip_steps_while_restoring_background
   assert(@vm.directory_exist?("/home/#{$live_user}/#{repo}/.git"))
   assert(@vm.file_exist?("/home/#{$live_user}/#{repo}/.git/config"))
-  @vm.execute_successfully("cd /home/#{$live_user}/#{repo}/ && git status", $live_user)
+  @vm.execute_successfully("cd '/home/#{$live_user}/#{repo}/' && git status", $live_user)
 end
