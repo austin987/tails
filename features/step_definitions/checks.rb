0508ad6ab22eda94baceeead789098efc6a1b002
Tails developers <amnesia@boum.org>
2015-02-12 14:26:52 +0000

    Add missing shell quoting.

--- features/step_definitions/checks.rb
+++ features/step_definitions/checks.rb
@@ -108,14 +108,14 @@ end
 Then /^there is no screenshot in the live user's home directory$/ do
   next if @skip_steps_while_restoring_background
   home = "/home/#{$live_user}"
-  @vm.execute("find #{home} -name 'Screenshot*.png' -maxdepth 1").stdout.empty?
+  @vm.execute("find '#{home}' -name 'Screenshot*.png' -maxdepth 1").stdout.empty?
 end
 
 Then /^a screenshot is saved to the live user's home directory$/ do
   next if @skip_steps_while_restoring_background
   home = "/home/#{$live_user}"
   try_for(3, :msg=> "No screenshot was created in #{home}") {
-    !@vm.execute("find #{home} -name 'Screenshot*.png' -maxdepth 1").stdout.empty?
+    !@vm.execute("find '#{home}' -name 'Screenshot*.png' -maxdepth 1").stdout.empty?
   }
 end
 
