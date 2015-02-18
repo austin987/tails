a2e59fe3fc5cf8f70a8733dc8c97616f8bdd71a6
Tails developers <tails@boum.org>
2015-02-18 11:01:43 +0000

    Modify assert (s/>/>=/)

--- features/support/helpers/storage_helper.rb
+++ features/support/helpers/storage_helper.rb
@@ -75,8 +75,8 @@ class VMStorage
     extrapadding = 500
     needed = convert_to_MiB(options[:size].to_i, options[:unit]) + extrapadding
     avail = convert_to_MiB(get_free_space('host', @pool_path), "KiB")
-    assert(avail > needed, "Error creating disk \"#{name}\" in \"#{@pool_path}\". Need " +
-                           "#{needed} MiB but only #{avail} MiB is available.")
+    assert(avail >= needed, "Error creating disk \"#{name}\" in \"#{@pool_path}\"." +
+                            "Need #{needed} MiB but only #{avail} MiB is available.")
 
     begin
       old_vol = @pool.lookup_volume_by_name(name)
