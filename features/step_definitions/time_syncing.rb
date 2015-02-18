3cc8bd0fcc271526f71cf17977917c63b49ee347
Tails developers <amnesia@boum.org>
2015-02-18 16:21:08 +0100

    Bump allowed clock drift after setting date to 5 seconds.
    In particular in nested VMs it seems 1 second wasn't enough.

--- features/step_definitions/time_syncing.rb
+++ features/step_definitions/time_syncing.rb
@@ -1,10 +1,17 @@
+# In some steps below we allow some slack when verifying that the date
+# was set appropriately because it may take time to send the `date`
+# command over the remote shell and get the answer back, parsing and
+# post-processing of the result, etc.
+def max_time_drift
+  5
+end
+
 When /^I set the system time to "([^"]+)"$/ do |time|
   next if @skip_steps_while_restoring_background
   @vm.execute_successfully("date -s '#{time}'")
   new_time = DateTime.parse(@vm.execute_successfully("date").stdout).to_time
-  # Allow 1 seconds delays.
   expected_time_lower_bound = DateTime.parse(time).to_time
-  expected_time_upper_bound = expected_time_lower_bound + 1
+  expected_time_upper_bound = expected_time_lower_bound + max_time_drift
   assert(expected_time_lower_bound >= new_time &&
                     new_time <= expected_time_upper_bound,
          "The guest's time was supposed to be set to " \
@@ -16,10 +23,9 @@ When /^I bump the system time with "([^"]+)"$/ do |timediff|
   old_time = DateTime.parse(@vm.execute_successfully("date").stdout).to_time
   @vm.execute_successfully("date -s 'now #{timediff}'")
   new_time = DateTime.parse(@vm.execute_successfully("date").stdout).to_time
-  # Allow 1 seconds delays.
   expected_time_lower_bound = DateTime.parse(
       cmd_helper("date -d '#{old_time} #{timediff}'")).to_time
-  expected_time_upper_bound = expected_time_lower_bound + 1
+  expected_time_upper_bound = expected_time_lower_bound + max_time_drift
   assert(expected_time_lower_bound >= new_time &&
                     new_time <= expected_time_upper_bound,
          "The guest's time was supposed to be bumped to " \
