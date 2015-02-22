85aafcd2e6ebe15d9bd90762efb5bc43b6488945
Tails developers <tails@boum.org>
2015-02-22 14:23:01 +0000

    Extend Git SSH steps to support standard SSH servers.

--- features/step_definitions/ssh.rb
+++ features/step_definitions/ssh.rb
@@ -1,17 +1,36 @@
-Given /^I have the SSH key pair for a Git repository$/ do
+Given /^I have the SSH key pair for an? (Git repository|SSH server)(?: on the| on a)?\s*(|LAN|Internet)$/ do |server_type, loc|
   next if @skip_steps_while_restoring_background
   @vm.execute_successfully("install -m 0700 -d '/home/#{LIVE_USER}/.ssh/'", LIVE_USER)
-  secret_ssh_key = $config["Unsafe_SSH_private_key"]
-  public_ssh_key = $config["Unsafe_SSH_public_key"]
-  assert(!secret_ssh_key.nil? && secret_ssh_key.length > 0)
-  assert(!public_ssh_key.nil? && public_ssh_key.length > 0)
+  case server_type
+  when "SSH server"
+    prefix = ''
+  when "Git repository"
+    prefix = "Unsafe_"
+  else
+    raise "Unknown server type #{server_type}"
+  end
+
+  case loc
+  when 'Internet', ''
+    location = '_'
+  when 'LAN'
+    location = '_LAN_'
+  else
+    raise "Unknown location #{loc} specified."
+  end
+  secret_ssh_key = $config[prefix + "SSH#{location}private_key"]
+  public_ssh_key = $config[prefix + "SSH#{location}public_key"]
+  assert(!secret_ssh_key.nil? && secret_ssh_key.length > 0, "no key in SSH#{location}private_key ?")
+  assert(!public_ssh_key.nil? && public_ssh_key.length > 0, "no key in SH#{location}public_key ?")
   @vm.execute_successfully("echo '#{secret_ssh_key}' > '/home/#{LIVE_USER}/.ssh/id_rsa'", LIVE_USER)
   @vm.execute_successfully("echo '#{public_ssh_key}' > '/home/#{LIVE_USER}/.ssh/id_rsa.pub'", LIVE_USER)
   @vm.execute_successfully("chmod 0600 '/home/#{LIVE_USER}/.ssh/'id*", LIVE_USER)
 end
 
-Given /^I verify the SSH fingerprint for the Git repository$/ do
+Given /^I verify the SSH fingerprint for the (Git repository|SSH server)$/ do |server_type|
   next if @skip_steps_while_restoring_background
-  @screen.wait("GitSSHFingerprint.png", 60)
+  assert(server_type = 'Git repository' || server_type = 'SSH server',
+         "Unknown server type #{server_type} specified.")
+  @screen.wait("SSHFingerprint.png", 60)
   @screen.type('yes' + Sikuli::Key.ENTER)
 end
