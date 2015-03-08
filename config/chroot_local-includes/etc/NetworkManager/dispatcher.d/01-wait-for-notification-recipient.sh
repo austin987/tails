5771f243a5442760d2a12feef28e8caf67801b4e
intrigeri <intrigeri@boum.org>
2015-03-08 09:28:20 +0000

    Wait for ibus-daemon instead of nm-applet, to judge whether we can send notifications.
    We should not be running nm-applet on GNOME Shell, since this functionality is
    nowadays taken over by GNOME Shell itself. And notifications are also handled by
    GNOME Shell, so let's instead wait for a process that's started by GNOME Shell,
    assuming that if it's ready enough to start long-lived child processes,
    hopefully it's ready to receive and enqueue/display notifications.
    
    Will-fix: #8685 (not really, but let's help Redmine track the relevant commits)

--- config/chroot_local-includes/etc/NetworkManager/dispatcher.d/01-wait-for-notification-recipient.sh
+++ config/chroot_local-includes/etc/NetworkManager/dispatcher.d/01-wait-for-notification-recipient.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# When a non-loopback interface comes up, wait for the Live user's nm-applet
+# When a non-loopback interface comes up, wait for the Live user's GNOME Shell
 # to come up. Wait 120 times one second maximum.
 
 [ "$1" != "lo" ] || exit 0
@@ -12,7 +12,7 @@ MAX_WAIT=120
 . /etc/live/config.d/username.conf
 
 for i in $(seq 1 ${MAX_WAIT}) ; do
-   if pgrep -u "${LIVE_USERNAME}" nm-applet >/dev/null 2>&1 ; then
+   if pgrep -u "${LIVE_USERNAME}" '^ibus-daemon' >/dev/null 2>&1 ; then
       break
    fi
    sleep 1
