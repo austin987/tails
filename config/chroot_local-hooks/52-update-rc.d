76be86f204377e251e491571938ee9ecabe736fb
intrigeri <intrigeri@boum.org>
2015-03-28 19:22:35 +0000

    Create a tails-wait-until-tor-has-bootstrapped.service in the systemd user instance.
    The systemd user and system instances don't share units. Thus, the dependency
    we have from the security check and upgrader user services on
    tails-wait-until-tor-has-bootstrapped had no effect.
    
    So, we have the *system* tails-wait-until-tor-has-bootstrapped.service create
    a file upon completion -- and the identically named *user* unit file wait for
    that file to appear.

--- config/chroot_local-hooks/52-update-rc.d
+++ config/chroot_local-hooks/52-update-rc.d
@@ -51,6 +51,7 @@ systemctl --global enable tails-create-tor-browser-directories.service
 systemctl --global enable tails-security-check.service
 systemctl --global enable tails-upgrade-frontend.service
 systemctl --global enable tails-virt-notify-user.service
+systemctl --global enable tails-wait-until-tor-has-bootstrapped.service
 systemctl --global enable tails-warn-about-disabled-persistence.service
 
 # Use socket activation only, to save a bit of memory and boot time
