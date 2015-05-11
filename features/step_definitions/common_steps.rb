c71c761c51824e8e121b88cc14537d1bbbf1c0f8
kytv <killyourtv@i2pmail.org>
2015-05-11 11:33:57 +0000

    Bump timeout
    The timeout that I originally set is too small, for example, when capturing is
    enabled.

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -298,7 +298,7 @@ Given /^the Tails desktop is ready$/ do
     # We wait for the Florence icon to be displayed to ensure reliable systray icon clicking.
     # By this point the only icon left is Vidalia and it will not cause the other systray
     # icons to shift positions.
-    @screen.wait("GnomeSystrayFlorence.png", 60)
+    @screen.wait("GnomeSystrayFlorence.png", 180)
   end
   @screen.wait(desktop_started_picture, 180)
 end
