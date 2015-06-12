c920840b927c701a561e5cba6bb059ad92e3eafe
intrigeri <intrigeri@boum.org>
2015-06-12 21:10:18 +0000

    Test suite: move initialization to the Given step, and the sanity check to where we actually need it.

--- features/step_definitions/po.rb
+++ features/step_definitions/po.rb
@@ -1,8 +1,8 @@
 Given /^I am in the Git branch being tested$/ do
-  File.exists?("#{GIT_DIR}/wiki/src/contribute/l10n_tricks/check_po.sh")
+  Dir.chdir(GIT_DIR)
 end
 
 Given /^all the PO files should be correct$/ do
-  Dir.chdir(GIT_DIR)
+  File.exists?("#{GIT_DIR}/wiki/src/contribute/l10n_tricks/check_po.sh")
   cmd_helper(['./wiki/src/contribute/l10n_tricks/check_po.sh'])
 end
