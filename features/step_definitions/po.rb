edf82f8f91db37965466ad9f5dcf07803319eae1
intrigeri <intrigeri@boum.org>
2015-06-12 21:12:00 +0000

    Test suite: use check_po from its new canonical location.

--- features/step_definitions/po.rb
+++ features/step_definitions/po.rb
@@ -3,6 +3,6 @@ Given /^I am in the Git branch being tested$/ do
 end
 
 Then /^all the PO files should be correct$/ do
-  File.exists?("#{GIT_DIR}/wiki/src/contribute/l10n_tricks/check_po.sh")
-  cmd_helper(['./wiki/src/contribute/l10n_tricks/check_po.sh'])
+  File.exists?('./submodules/jenkins-tools/slaves/check_po')
+  cmd_helper(['./submodules/jenkins-tools/slaves/check_po'])
 end
