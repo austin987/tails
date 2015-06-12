6a5f90dcc08eb0a846af12b84c8e6f066e0f6234
intrigeri <intrigeri@boum.org>
2015-06-12 21:10:58 +0000

    Test suite: rename Given step to Then, that's how we use it.

--- features/step_definitions/po.rb
+++ features/step_definitions/po.rb
@@ -2,7 +2,7 @@ Given /^I am in the Git branch being tested$/ do
   Dir.chdir(GIT_DIR)
 end
 
-Given /^all the PO files should be correct$/ do
+Then /^all the PO files should be correct$/ do
   File.exists?("#{GIT_DIR}/wiki/src/contribute/l10n_tricks/check_po.sh")
   cmd_helper(['./wiki/src/contribute/l10n_tricks/check_po.sh'])
 end
