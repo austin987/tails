cc729b82708d443765b2ed0dc247e1c619ca935a
anonym <anonym@riseup.net>
2015-09-15 21:56:17 +0200

    Allow '+' and ':' in video capture filenames.
    This makes timestamps and timezone info easier to read.

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -114,7 +114,7 @@ Before('@product') do |scenario|
   if $config["CAPTURE"]
     video_name = "capture-" + "#{scenario.name}-#{TIME_AT_START}.mkv"
     # Sanitize the filename from unix-hostile filename characters
-    bad_filename_chars = Regexp.new("[^A-Za-z0-9_\\-.,]")
+    bad_filename_chars = Regexp.new("[^A-Za-z0-9_\\-.,+:]")
     video_name.gsub!(bad_filename_chars, '_')
     @video_path = "#{$config['TMPDIR']}/#{video_name}"
     capture = IO.popen(['avconv',
