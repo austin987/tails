eab3a6e02295be17ec16295fafb23746fa4a1e86
anonym <anonym@riseup.net>
2015-09-18 15:57:22 +0200

    Simplify accessibility notification for Restart/Power Off.
    So the screen reader says simply "Restart" instead of "Restart Push
    Button".

--- config/chroot_local-includes/usr/share/gnome-shell/extensions/shutdown-helper@tails.boum.org/extension.js
+++ config/chroot_local-includes/usr/share/gnome-shell/extensions/shutdown-helper@tails.boum.org/extension.js
@@ -23,6 +23,7 @@
 const Lang = imports.lang;
 const Mainloop = imports.mainloop;
 
+const Atk = imports.gi.Atk;
 const LoginManager = imports.misc.loginManager;
 const Main = imports.ui.main;
 const StatusSystem = imports.ui.status.system;
@@ -69,9 +70,13 @@ const Extension = new Lang.Class({
 
     _createActions: function() {
         this._altrestartAction = this.systemMenu._createActionButton('view-refresh-symbolic', _("Restart"));
+        this._altrestartAction.set_accessible_role(Atk.Role.UNKNOWN);
+
         this._altrestartActionId = this._altrestartAction.connect('clicked', Lang.bind(this, this._onRestartClicked));
 
         this._altpowerOffAction = this.systemMenu._createActionButton('system-shutdown-symbolic', _("Power Off"));
+        this._altpowerOffAction.set_accessible_role(Atk.Role.UNKNOWN);
+
         this._altpowerOffActionId = this._altpowerOffAction.connect('clicked', Lang.bind(this, this._onPowerOffClicked));
     },
 
