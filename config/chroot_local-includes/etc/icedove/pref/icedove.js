169f9f940fbc86f7343119d253dc9c2d11ac2fe7
kytv <killyoutv@i2pmail.org>
2015-10-02 16:27:28 +0000

    Remove configurations handled by Torbirdy

--- config/chroot_local-includes/etc/icedove/pref/icedove.js
+++ config/chroot_local-includes/etc/icedove/pref/icedove.js
@@ -19,32 +19,5 @@ pref("mail.shell.checkDefaultMail", false);
 pref("network.protocol-handler.app.http", "x-www-browser");
 pref("network.protocol-handler.app.https", "x-www-browser");
 
-// Stop leaks in the HELO/EHLO headers
-pref("mail.smtpserver.default.hello_argument", "localhost");
-
-// Stop DNS leaks
-pref("network.proxy.socks_remote_dns", True);
-
-// Disable HTML when showing message bodies
-pref("mailnews.display.disallow_mime_handlers", 1);
-pref("mailnews.display.html_as", 1);
-pref("mailnews.display.prefer_plaintext", True);
-
-// Disable HTML email composing
-pref("mail.html_compose", False);
-pref("mail.identity.default.compose_html", False);
-pref("mail.default_html_action", 1);
-
-// Set general.useragent.override equal to that of the most recent Thunderbird from the time of the firefox version specified in current Torbutton.
-// FIXME: Can we script this? Can getTorbuttonUserAgent help some how?
-// Below, useragent of windows user with Thunderbird version released the day before Firefox 3.6.3. Data gathered from: http://www.useragentstring.com
-// FIXME: Should we specify LANG
-pref("general.useragent.override", "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.9) Gecko/20100317 Thunderbird/3.0.4");
-
-// Avoid leaking Enigmail usage and version in X-Enigmail-Version header
-pref("extensions.enigmail.addHeaders", False);
-// Avoid leaking Enigmail usage and version in message
-pref("extensions.enigmail.useDefaultComment", True);
-
 // Tell TorBirdy we're running Tails so that it adapts its behaviour.
 pref("vendor.name", "Tails");
