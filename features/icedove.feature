2a05e0b5300de4ba6a7004b851cf1c030886d38e
kytv <killyoutv@i2pmail.org>
2015-10-03 21:15:57 +0000

    Initial set of tests for Icedove
    Will-fix: #10332

---
+++ features/icedove.feature
@@ -0,0 +1,51 @@
+@product @check_tor_leaks
+Feature: Icedove email client
+  As a Tails user
+  I may want to use an email client
+
+  Background:
+    Given a computer
+    When I start Tails from DVD and I login
+    And I save the state so the background can be restored next scenario
+
+  Scenario: Torbirdy is enabled and configured to use Tor
+    Given I start "Icedove" via the GNOME "Internet" applications menu
+    And Icedove has started
+    Then I see that Torbirdy is enabled and configured to use Tor
+
+  Scenario: Icedove defaults to using IMAP
+    Given I start "Icedove" via the GNOME "Internet" applications menu
+    And Icedove has started
+    And I have not configured an email account
+    Then I am prompted to setup an email account
+    And IMAP is the default protocol
+
+  Scenario: Adblock is not enabled within Icedove
+    Given I start "Icedove" via the GNOME "Internet" applications menu
+    And Icedove has started
+    And I have not configured an email account
+    And I cancel setting up an email account
+    When I open Icedove's Add-ons Manager
+    Then the Add-ons Manager opens
+    When I click the extensions tab
+    Then I see that Adblock is not installed in Icedove
+
+  Scenario: Enigmail is configured to use the correct keyserver
+    Given I start "Icedove" via the GNOME "Internet" applications menu
+    And Icedove has started
+    And I have not configured an email account
+    And I cancel setting up an email account
+    And I go into Enigmail's preferences
+    When I click Enigmail's keyserver tab
+    Then I see Enigmail is configured to use the correct keyserver
+    When I click Enigmail's advanced tab
+    Then I see that Enigmail is configured to use the correct SOCKS proxy
+
+  Scenario: Icedove will work over Tor
+    Given I start "Icedove" via the GNOME "Internet" applications menu
+    And Icedove has started
+    And I have not configured an email account
+    And I cancel setting up an email account
+    And I open Torbirdy's preferences
+    When I test Torbirdy's proxy settings
+    Then Torbirdy's proxy test is successful
---
+++ features/step_definitions/icedove.rb
@@ -0,0 +1,111 @@
+Then /^Icedove has started$/ do
+  next if @skip_steps_while_restoring_background
+  step 'process "icedove" is running within 30 seconds'
+  @screen.wait('IcedoveMainWindow.png', 60)
+end
+
+When /^I have not configured an email account$/ do
+  next if @skip_steps_while_restoring_background
+  icedove_prefs = @vm.file_content("/home/#{LIVE_USER}/.icedove/profile.default/prefs.js").chomp
+  assert(!icedove_prefs.include?('mail.accountmanager.accounts'))
+end
+
+Then /^I am prompted to setup an email account$/ do
+  next if @skip_steps_while_restoring_background
+  @vm.focus_window('Mail Account Setup')
+  @screen.wait('IcedoveMailAccountSetup.png', 30)
+end
+
+Then /^IMAP is the default protocol$/ do
+  next if @skip_steps_while_restoring_background
+  @vm.focus_window('Mail Account Setup')
+  @screen.wait('IcedoveProtocolIMAP.png', 10)
+end
+
+Then /^I cancel setting up an email account$/ do
+  next if @skip_steps_while_restoring_background
+  @vm.focus_window('Mail Account Setup')
+  @screen.type(Sikuli::Key.ESC)
+  @screen.waitVanish('IcedoveMailAccountSetup.png', 10)
+end
+
+Then /^I open Icedove's Add-ons Manager$/ do
+  next if @skip_steps_while_restoring_background
+  @vm.focus_window('Icedove')
+  @screen.type("t", Sikuli::KeyModifier.ALT)
+  @screen.wait_and_click('IcedoveToolsMenuAddOns.png', 10)
+end
+
+Then /^the Add-ons Manager opens$/ do
+  next if @skip_steps_while_restoring_background
+  @screen.wait('MozillaAddonsManagerExtensions.png', 30)
+end
+
+Then /^I click the extensions tab$/ do
+  next if @skip_steps_while_restoring_background
+  @screen.wait_and_click('MozillaAddonsManagerExtensions.png', 10)
+end
+
+Then /^I see that Adblock is not installed in Icedove$/ do
+  next if @skip_steps_while_restoring_background
+  if @screen.exists('MozillaExtensionsAdblockPlus.png')
+    raise 'Adblock should not be enabled within Icedove'
+  end
+end
+
+Then /^I see that Torbirdy is enabled and configured to use Tor$/ do
+  next if @skip_steps_while_restoring_background
+  @screen.wait('IcedoveTorbirdyEnabled.png', 10)
+end
+
+When /^I go into Enigmail's preferences$/ do
+  next if @skip_steps_while_restoring_background
+  @vm.focus_window('Icedove')
+  @screen.type("a", Sikuli::KeyModifier.ALT)
+  @screen.wait_and_click('IcedoveEnigmailPreferences.png', 10)
+  @screen.wait('IcedoveEnigmailPreferencesWindow.png', 10)
+  @screen.click('IcedoveEnigmailExpertSettingsButton.png')
+  @screen.wait('IcedoveEnigmailKeyserverTab.png', 10)
+end
+
+When /^I click Enigmail's keyserver tab$/ do
+  next if @skip_steps_while_restoring_background
+  @screen.wait_and_click('IcedoveEnigmailKeyserverTab.png', 10)
+end
+
+Then /^I see Enigmail is configured to use the correct keyserver$/ do
+  next if @skip_steps_while_restoring_background
+  @screen.wait('IcedoveEnigmailKeyserver.png', 10)
+end
+
+Then /^I click Enigmail's advanced tab$/ do
+  next if @skip_steps_while_restoring_background
+  @screen.wait_and_click('IcedoveEnigmailAdvancedTab.png', 10)
+end
+
+Then /^I see that Enigmail is configured to use the correct SOCKS proxy$/ do
+  next if @skip_steps_while_restoring_background
+  @screen.click('IcedoveEnigmailAdvancedParameters.png')
+  @screen.type(Sikuli::Key.END)
+  @screen.wait('IcedoveEnigmailProxy.png', 10)
+end
+
+When /^I open Torbirdy's preferences$/ do
+  next if @skip_steps_while_restoring_background
+  @screen.wait_and_click('IcedoveTorbirdyEnabled.png', 10)
+  @screen.wait_and_click('IcedoveTorbirdyPreferences.png', 10)
+  @screen.wait('GnomeQuestionDialogIcon.png', 10)
+  @screen.type(Sikuli::Key.ENTER)
+end
+
+When /^I test Torbirdy's proxy settings$/ do
+  next if @skip_steps_while_restoring_background
+  @screen.wait('IcedoveTorbirdyPreferencesWindow.png', 10)
+  @screen.click('IcedoveTorbirdyTestProxySettingsButton.png')
+  @screen.wait('IcedoveTorbirdyCongratulationsTab.png', 180)
+end
+
+Then /^Torbirdy's proxy test is successful$/ do
+  next if @skip_steps_while_restoring_background
+  @screen.wait('IcedoveTorbirdyCongratulationsTab.png', 180)
+end
