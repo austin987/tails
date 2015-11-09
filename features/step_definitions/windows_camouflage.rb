050160e84c0cabc63d1b46edd33a68c50520ae2b
anonym <anonym@riseup.net>
2015-11-09 23:52:38 +0100

    Remove all automated tests of the Windows camouflage.
    We don't have a working Windows camouflage in Tails/Jessie. If it's
    eventually implemented we, should restore:
    
    * features/windows_camouflage.feature, and
    * features/step_definitions/windows_camouflage.rb,
    
    possibly with some minor modifications. In all likelihood all images
    will have to be updated.
    
    Refs: #8064

--- features/step_definitions/windows_camouflage.rb
+++
@@ -1,8 +0,0 @@
-Given /^I enable Microsoft Windows camouflage$/ do
-  @theme = "windows"
-  @screen.wait_and_click("TailsGreeterWindowsCamouflage.png", 10)
-end
-
-When /^I click the start menu$/ do
-  @screen.wait_and_click("WindowsStartButton.png", 10)
-end
