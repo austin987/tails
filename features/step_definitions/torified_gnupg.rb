1a636927f1d79fc698c8a969852a1197ca5e63cd
kytv <killyourtv@i2pmail.org>
2015-11-13 04:02:38 +0000

    s/gsub/sub/

--- features/step_definitions/torified_gnupg.rb
+++ features/step_definitions/torified_gnupg.rb
@@ -209,5 +209,5 @@ Then /^Seahorse is configured to use the correct keyserver$/ do
   assert_equal(1, @gnome_keyservers.count, 'Seahorse should only have one keyserver configured.')
   # Seahorse doesn't support hkps so that part of the domain is stripped out.
   # We also insert hkp:// to the beginning of the domain.
-  assert_equal(CONFIGURED_KEYSERVER_HOSTNAME.gsub('hkps.', 'hkp://'), @gnome_keyservers[0])
+  assert_equal(CONFIGURED_KEYSERVER_HOSTNAME.sub('hkps.', 'hkp://'), @gnome_keyservers[0])
 end
