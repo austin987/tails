848f494286fa5abb667a1eb49799b9047fd2eaea
sajolida <sajolida@pimienta.org>
2015-11-17 14:30:11 +0000

    Synchronize with Giorgio's code

--- wiki/src/install/inc/js/dave.js
+++ wiki/src/install/inc/js/dave.js
@@ -1,5 +1,5 @@
 (function() {
-  var chromeSupported = !/\bchrome-unsupported\b/.test(document.documentElement.className);
+  var chromeSupported = !/\bchrome-unsupported\b/.test((document.getElementById("download-and-verify") || document.documentElement).className);
   var minVer = {
     "firefox": 38,
     "chrome": 44,
@@ -9,13 +9,29 @@
   function setBrowser(browser) {
     document.documentElement.dataset.browser = browser ? "sb-" + browser : "unsupported";
   }
+  function forId(id, cb) {
+    var el = document.getElementById(id);
+    if (el) cb(el);
+  }
+  var forClass = document.getElementsByClassName ? function(cn, cb) {
+    var ee = document.getElementsByClassName(cn);
+    for (var j = ee.length; j-- > 0;) cb(ee[j]);
+  } : function () {};
 
   var browser,
-      v =  navigator.userAgent.match(/\b(Chrome|Firefox)\/(\d+)/);
+      v =  navigator.userAgent.match(/\b(Chrome|Firefox)\/(\d+)/)
+      ;
   v = v && parseInt(v[2]) || 0;
-  if ("InstallTrigger" in window) {
-    if (v >= minVer.firefox)
+  if (window.InstallTrigger) {
+    if (v >= minVer.firefox || minVer.firefox === 38 && new RegExp("").source) // see Fx 38 RegExp @ https://developer.mozilla.org/it/Firefox/Releases/38
       browser = "firefox";
+    else {
+      addEventListener("DOMContentLoaded", function() {
+        forId("unsupported-other", function(el) { el.style.display = "none"; });
+        if (!v) v = "< 38";
+        forClass("current-firefox", function(el) { el.innerHTML = v || "< 38"; });
+      }, true);
+    }
   } else if (chromeSupported && /\bChrom/.test(navigator.userAgent) && /\bGoogle Inc\./.test(navigator.vendor)) {
     if (v >= minVer.chrome)
       browser = "chrome";
@@ -27,11 +43,9 @@
 
   addEventListener("load", function(ev) {
     style.parentNode.removeChild(style);
-    var ee, j;
+    forId("self-url", function (el) { el.textContent = location.href; });
     for (var browser in minVer) {
-      ee = document.getElementsByClassName("minver-" + browser);
-      for (j = ee.length; j-- > 0;)
-        ee[j].innerHTML = minVer[browser];
+      forClass("minver-" + browser, function(el) { el.innerHTML = minVer[browser]; });
     }
   }, true);
 
