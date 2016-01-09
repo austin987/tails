1cc688e2f919f1d3494926111c593abbbc43b1e1
intrigeri <intrigeri@boum.org>
2016-01-09 11:57:20 +0000

    Fix typo.
    refs: #9285

--- config/chroot_local-includes/etc/tor-browser/profile/preferences/0000tails.js
+++ config/chroot_local-includes/etc/tor-browser/profile/preferences/0000tails.js
@@ -98,7 +98,7 @@ pref("browser.search.geoSpecificDefaults", false);
 // the progress being made.
 pref("browser.download.panel.shown", true);
 
-// Give our AppArmor sandboxing, Tor Browser will not be allowed to
+// Given our AppArmor sandboxing, Tor Browser will not be allowed to
 // open external applications, so let's forbid it, only allowing
 // saving downloaded files.
 pref("browser.download.forbid_open_with", true);
