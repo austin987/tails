2ad597c657b1c185f9f5d313d8119420ccc0a07d
anonym <anonym@riseup.net>
2016-01-19 16:09:47 +0100

    Adapt packet counting code to the new iptables parser.

--- features/step_definitions/checks.rb
+++ features/step_definitions/checks.rb
@@ -247,18 +247,11 @@ When /^I disable all networking in the Tails Greeter$/ do
 end
 
 Then /^network traffic is (not )?generated during the Tails session$/ do |not_generated|
-  pkts = 0
-  iptables_output = $vm.execute_successfully("iptables -vnL").stdout.chomp
-  ['INPUT', 'OUTPUT', 'FORWARD', 'lan'].each do |chain|
-    iptables_parse(iptables_output)[chain]["rules"].each do |rule|
-      pkts += rule["pkts"]
-    end
-  end
-
+  pkts = ip4tables_packet_counter_sum
   if not_generated
     assert_equal(0, pkts)
   else
     assert_not_equal(0, pkts)
   end
-  debug_log("#{pkts} packets found.", :color => :green)
+  debug_log("#{pkts} packets found")
 end
