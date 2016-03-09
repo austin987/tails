4fb93234d0adc593563712384092e647c5769466
anonym <anonym@riseup.net>
2016-03-09 03:35:49 +0100

    Enable Icedove's automatic configuration wizard.
    TorBirdy disables it due to both its use of insecure protocols for
    various lookups, and that it can result in unsafe
    configurations. However, with our patches applied, it can be
    configured to only use secure protocols, so the wizard is safe again.
    
    Will-fix: #6158

--- config/chroot_local-includes/etc/skel/.icedove/profile.default/preferences/0000tails.js
+++ config/chroot_local-includes/etc/skel/.icedove/profile.default/preferences/0000tails.js
@@ -1,1 +1,9 @@
 user_pref("extensions.enigmail.configuredVersion", "1.8.2");
+
+// TorBirdy tries to set values on the first run which forces us to
+// set the following unless we want our settings of it to be
+// overwritten...
+user_pref("extensions.torbirdy.first_run", false);a
+// ... and for the same reason we cannot store the following value in
+// the global "seed" preferences, /etc/icedove/pref/icedove.js.
+user_pref("extensions.torbirdy.emailwizard", true);
