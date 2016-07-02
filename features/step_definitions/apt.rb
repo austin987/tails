9c562d3161ede2a94b542e0da3ef1ffb6f2d6439
bertagaz <bertagaz@ptitcanardnoir.org>
2016-07-02 15:08:08 +0200

    Use retry_tor on the apt install scenario.
    Refs: #10496

--- features/step_definitions/apt.rb
+++ features/step_definitions/apt.rb
@@ -25,10 +25,18 @@ end
 
 Then /^I should be able to install a package using apt$/ do
   package = "cowsay"
-  Timeout::timeout(120) do
-    $vm.execute_successfully("echo #{@sudo_password} | " +
-                             "sudo -S apt install #{package}",
-                             :user => LIVE_USER)
+  recovery_proc = Proc.new do
+    step 'I kill the process "apt"' if $vm.has_process?("apt")
+    $vm.execute("echo #{@sudo_password} | " +
+                "sudo -S apt purge #{package}",
+                :user => LIVE_USER)
+  end
+  retry_tor(recovery_proc) do
+    Timeout::timeout(120) do
+      $vm.execute_successfully("echo #{@sudo_password} | " +
+                               "sudo -S apt install #{package}",
+                               :user => LIVE_USER)
+    end
   end
   step "package \"#{package}\" is installed"
 end
