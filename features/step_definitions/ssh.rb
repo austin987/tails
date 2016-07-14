9a9ad1487a514e6c5065caa450ce012df8bb90db
bertagaz <bertagaz@ptitcanardnoir.org>
2016-07-14 14:16:32 +0200

    Revert "Wording."
    This reverts commit 273dbe2e3490ef45e6733bc8fec6db35215affa7.

--- features/step_definitions/ssh.rb
+++ features/step_definitions/ssh.rb
@@ -144,11 +144,11 @@ Then /^I connect to an SFTP server on the Internet$/ do
     @screen.wait("GnomeConnectToServerWindow.png", 10)
     @screen.type("sftp://" + @sftp_username + "@" + @sftp_host + ":" + @sftp_port)
     @screen.wait_and_click("GnomeConnectToServerConnectButton.png", 10)
-    step "I verify the SSH fingerprint of the SFTP server"
+    step "I verify the SSH fingerprint for the SFTP server"
   end
 end
 
-Then /^I verify the SSH fingerprint of the SFTP server$/ do
+Then /^I verify the SSH fingerprint for the SFTP server$/ do
   result, _ = @screen.waitAny(["GnomeSSHPermissionError.png",
                               "GnomeSSHProcessExit.png",
                               "GnomeSSHVerificationConfirm.png"],
