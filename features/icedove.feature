865f32a7ad415da6c324e2e7c19ade56af06e321
anonym <anonym@riseup.net>
2016-09-01 23:38:17 +0200

    Dogtailify existing Icedove tests.
    ... which incidentally fixes the issue we've had with some outdated test
    suite images for this feature, and a robustness issue in TorBirdy's
    settings dialog. :)
    
    Also, by removing the IMAP scenario, which has been broken ever since we
    enabled the autoconfiguration wizard, we fix another failure -- we'll
    reintroduce an updated version of it shortly.
    
    Will-fix: #11571, #11400

--- features/icedove.feature
+++ features/icedove.feature
@@ -11,9 +11,6 @@ Feature: Icedove email client
     And I have not configured an email account
     Then I am prompted to setup an email account
 
-  Scenario: Icedove defaults to using IMAP
-    Then IMAP is the default protocol
-
   Scenario: Adblock is not enabled within Icedove
     Given I cancel setting up an email account
     When I open Icedove's Add-ons Manager
@@ -23,12 +20,12 @@ Feature: Icedove email client
   Scenario: Enigmail is configured to use the correct keyserver
     Given I cancel setting up an email account
     And I go into Enigmail's preferences
-    When I click Enigmail's keyserver tab
+    And I enable Enigmail's expert settings
+    When I click Enigmail's Keyserver tab
     Then I see that Enigmail is configured to use the correct keyserver
-    When I click Enigmail's advanced tab
+    When I click Enigmail's Advanced tab
     Then I see that Enigmail is configured to use the correct SOCKS proxy
 
   Scenario: Torbirdy is configured to use Tor
     Given I cancel setting up an email account
-    And I open Torbirdy's preferences
     Then I see that Torbirdy is configured to use Tor
--- features/step_definitions/icedove.rb
+++ features/step_definitions/icedove.rb
@@ -1,6 +1,17 @@
+def icedove_app
+  Dogtail::Application.new('Icedove')
+end
+
+def icedove_main
+  icedove_app.child('Home - Icedove Mail/News', roleName: 'frame')
+end
+
+def icedove_wizard
+  icedove_app.child('Mail Account Setup', roleName: 'frame')
+end
+
 Then /^Icedove has started$/ do
-  step 'process "icedove" is running within 30 seconds'
-  @screen.wait('IcedoveMainWindow.png', 60)
+  icedove_main.wait(60)
 end
 
 When /^I have not configured an email account$/ do
@@ -9,76 +20,67 @@ When /^I have not configured an email account$/ do
 end
 
 Then /^I am prompted to setup an email account$/ do
-  $vm.focus_window('Mail Account Setup')
-  @screen.wait('IcedoveMailAccountSetup.png', 30)
-end
-
-Then /^IMAP is the default protocol$/ do
-  $vm.focus_window('Mail Account Setup')
-  @screen.wait('IcedoveProtocolIMAP.png', 10)
+  icedove_wizard.wait(30)
 end
 
 Then /^I cancel setting up an email account$/ do
-  $vm.focus_window('Mail Account Setup')
-  @screen.type(Sikuli::Key.ESC)
-  @screen.waitVanish('IcedoveMailAccountSetup.png', 10)
+  icedove_wizard.button('Cancel').click
 end
 
 Then /^I open Icedove's Add-ons Manager$/ do
-  $vm.focus_window('Icedove')
-  @screen.wait_and_click('MozillaMenuButton.png', 10)
-  @screen.wait_and_click('IcedoveToolsMenuAddOns.png', 10)
-  @screen.wait('MozillaAddonsManagerExtensions.png', 30)
+  icedove_main.button('AppMenu').click
+  icedove_main.child('Add-ons', roleName: 'menu item').click
+  @icedove_addons = icedove_app.child(
+    'Add-ons Manager - Icedove Mail/News', roleName: 'frame'
+  )
+  @icedove_addons.wait
 end
 
 Then /^I click the extensions tab$/ do
-  @screen.wait_and_click('MozillaAddonsManagerExtensions.png', 10)
+  @icedove_addons.child('Extensions', roleName: 'list item').click
 end
 
 Then /^I see that Adblock is not installed in Icedove$/ do
-  if @screen.exists('MozillaExtensionsAdblockPlus.png')
-    raise 'Adblock should not be enabled within Icedove'
+  extensions_list = @icedove_addons.child('amnesia branding', roleName: 'label')
+                    .parent.parent
+  adblock_installed = extensions_list.children(roleName: 'label').any? do |label|
+    label.text.match(/adblock/i)
   end
+  assert(not(adblock_installed))
 end
 
 When /^I go into Enigmail's preferences$/ do
   $vm.focus_window('Icedove')
   @screen.type("a", Sikuli::KeyModifier.ALT)
-  @screen.wait_and_click('IcedoveEnigmailPreferences.png', 10)
-  @screen.wait('IcedoveEnigmailPreferencesWindow.png', 10)
-  @screen.click('IcedoveEnigmailExpertSettingsButton.png')
-  @screen.wait('IcedoveEnigmailKeyserverTab.png', 10)
+  icedove_main.child('Preferences', roleName: 'menu item').click
+  @enigmail_prefs = icedove_app.dialog('Enigmail Preferences')
 end
 
-When /^I click Enigmail's keyserver tab$/ do
-  @screen.wait_and_click('IcedoveEnigmailKeyserverTab.png', 10)
+When /^I enable Enigmail's expert settings$/ do
+  @enigmail_prefs.button('Display Expert Settings and Menus').click
+end
+
+Then /^I click Enigmail's (.+) tab$/ do |tab_name|
+  @enigmail_prefs.child(tab_name, roleName: 'page tab').click
 end
 
 Then /^I see that Enigmail is configured to use the correct keyserver$/ do
-  @screen.wait('IcedoveEnigmailKeyserver.png', 10)
-end
-
-Then /^I click Enigmail's advanced tab$/ do
-  @screen.wait_and_click('IcedoveEnigmailAdvancedTab.png', 10)
+  keyservers = @enigmail_prefs.child(
+    'Specify your keyserver(s):', roleName: 'entry'
+  ).text
+  assert_equal('hkps://hkps.pool.sks-keyservers.net', keyservers)
 end
 
 Then /^I see that Enigmail is configured to use the correct SOCKS proxy$/ do
-  @screen.click('IcedoveEnigmailAdvancedParameters.png')
-  @screen.type(Sikuli::Key.END)
-  @screen.wait('IcedoveEnigmailProxy.png', 10)
+  gnupg_parameters = @enigmail_prefs.child(
+    'Additional parameters for GnuPG', roleName: 'entry'
+  ).text
+  assert_not_nil(
+    gnupg_parameters['--keyserver-options http-proxy=socks5h://127.0.0.1:9050']
+  )
 end
 
 Then /^I see that Torbirdy is configured to use Tor$/ do
-  @screen.wait('IcedoveTorbirdyEnabled.png', 10)
-end
-
-When /^I open Torbirdy's preferences$/ do
-  step "I open Icedove's Add-ons Manager"
-  step 'I click the extensions tab'
-  @screen.wait_and_click('MozillaExtensionsTorbirdy.png', 10)
-  @screen.type(Sikuli::Key.TAB)   # Select 'More' link
-  @screen.type(Sikuli::Key.TAB)   # Select 'Preferences' button
-  @screen.type(Sikuli::Key.SPACE) # Press 'Preferences' button
-  @screen.wait('GnomeQuestionDialogIcon.png', 10)
-  @screen.type(Sikuli::Key.ENTER)
+  icedove_main.child(roleName: 'status bar')
+    .child('TorBirdy Enabled:    Tor', roleName: 'label').wait
 end
