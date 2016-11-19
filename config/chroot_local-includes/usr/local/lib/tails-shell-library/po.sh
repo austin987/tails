b34237e0ff61cd37c8141e769616f6d30aef77ba
Ulrike Uhlig <u@451f.org>
2016-11-19 22:20:10 +0100

    We don't need a semicolon here.

--- config/chroot_local-includes/usr/local/lib/tails-shell-library/po.sh
+++ config/chroot_local-includes/usr/local/lib/tails-shell-library/po.sh
@@ -21,15 +21,15 @@ compare_po_headers() {
     for locale in "$@" ; do
          if [ -f ${locale}.po.new ]; then
              echo "$locale file exists."
-         fi;
+         fi
          if [ $(diff "${locale}.po" "${locale}.po.new" | grep ^"> " | wc -l) -eq 1 ]; then
              if diff -aw "${locale}.po" "${locale}.po.new" | grep ^'> "POT-Creation-Date:'; then
                  echo "Only header changes in potfile, Delete new PO file."
                  rm -f ${locale}.po.new
-             fi;
+             fi
          else
              echo "Real changes in potfile: substitute old PO file."
              mv ${locale}.po.new ${locale}.po
-         fi;
+         fi
     done
 }
