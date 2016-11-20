070ea2fb9a1bb9aab015c5ec1d07d28accdb0c47
Ulrike Uhlig <u@451f.org>
2016-11-20 10:01:50 +0100

    Adding a parenthesis, so that at the end of the loop we get out of po/.
    Modify indentation.
    Modify test if a po file exists.
    Modify comments to be more verbose.

--- config/chroot_local-includes/usr/local/lib/tails-shell-library/po.sh
+++ config/chroot_local-includes/usr/local/lib/tails-shell-library/po.sh
@@ -17,22 +17,22 @@ intltool_update_po () {
 }
 
 keep_old_po_files_if_header_only_changed() {
+    (
     cd po
     for locale in "$@" ; do
-         if [ ! -f ${locale}.po.new ]; then
-             echo "New PO file for ${locale} does not exist. Skipping."
-             continue
-		else
-             echo "${locale} PO file exists."
-         fi
-         if [ $(diff "${locale}.po" "${locale}.po.new" | grep ^"> " | wc -l) -eq 1 ]; then
-             if diff -aw "${locale}.po" "${locale}.po.new" | grep ^'> "POT-Creation-Date:'; then
-                 echo "Only header changes in potfile, Delete new PO file."
-                 rm -f ${locale}.po.new
-             fi
-         else
-             echo "Real changes in potfile: substitute old PO file."
-             mv ${locale}.po.new ${locale}.po
-         fi
+        if [ ! -f ${locale}.po.new && ! -f ${locale}.po ]; then
+            echo "New PO file for ${locale} does not exist. Skipping."
+            continue
+        fi
+        if [ $(diff "${locale}.po" "${locale}.po.new" | grep ^"> " | wc -l) -eq 1 ]; then
+            if diff -aw "${locale}.po" "${locale}.po.new" | grep ^'> "POT-Creation-Date:'; then
+                echo "${locale}: Only header changes in potfile, Delete new PO file."
+                rm -f ${locale}.po.new
+            fi
+        else
+            echo "${locale}: Real changes in potfile: substitute old PO file."
+            mv ${locale}.po.new ${locale}.po
+        fi
     done
+    )
 }
