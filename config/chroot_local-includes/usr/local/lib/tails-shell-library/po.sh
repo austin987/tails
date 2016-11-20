6b78a0e4c2609f4ec147bf8378739d9c6fc229d6
Ulrike Uhlig <u@451f.org>
2016-11-20 12:21:16 +0100

    Improve regular expression.
    We should make sure that exactly 1 line is removed, exactly 1 line is
    added, and both those lines match the regexp
    ^'(?:>|<) "POT-Creation-Date:'

--- config/chroot_local-includes/usr/local/lib/tails-shell-library/po.sh
+++ config/chroot_local-includes/usr/local/lib/tails-shell-library/po.sh
@@ -17,8 +17,9 @@ intltool_update_po () {
                 echo "New PO file for ${locale} does not exist. Skipping."
                 continue
             fi
-            if [ $(diff "${locale}.po" "${locale}.po.new" | grep -c ^"> ") -eq 1 ]; then
-                if diff -aw "${locale}.po" "${locale}.po.new" | grep ^'> "POT-Creation-Date:'; then
+            if [ $(diff "${locale}.po" "${locale}.po.new") | grep -Ec ^"?>" -eq 1 && \
+                 $(diff "${locale}.po" "${locale}.po.new") | grep -Ec ^"?<" -eq 1 ]; then
+                 if diff "${locale}.po" "${locale}.po.new" | grep -E ^'(?:>|<) "POT-Creation-Date:'; then
                     echo "${locale}: Only header changes in potfile, delete new PO file."
                     rm ${locale}.po.new
                 fi
