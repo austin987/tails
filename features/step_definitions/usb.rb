cb536388b7122e3cec4b3b3366c76fb2ca3a29ef
anonym <anonym@riseup.net>
2016-11-29 20:47:29 +0100

    Use the correct variable.

--- features/step_definitions/usb.rb
+++ features/step_definitions/usb.rb
@@ -158,7 +158,7 @@ end
 
 Given /^I plug and mount a USB drive containing the Tails ISO$/ do
   iso_dir = share_host_files(TAILS_ISO)
-  @iso_path = "#{@iso_dir}/#{File.basename(TAILS_ISO)}"
+  @iso_path = "#{iso_dir}/#{File.basename(TAILS_ISO)}"
 end
 
 When /^I do a "Upgrade from ISO" on USB drive "([^"]+)"$/ do |name|
