austin987/tails

commit c6541323cb70f7c2d1af77bb407bef0d3d3e554a

Revert "Test suite: test that Totem cannot access files is ~/.gnupg via alternate, live-boot generated paths."
This reverts commit 2014066da02fd3103fd05dda2795d5e0f7b4c3c2.

These overlayfs branches are not exposed by live-boot.

features/totem.feature
@@ -25,21 +25,8 @@ Feature: Using Totem
     Then I see "TotemUnableToOpen.png" after at most 10 seconds
     And AppArmor has denied "/usr/bin/totem" from opening "/home/amnesia/.gnupg/video.mp4"
     Given I close Totem
-    And the file "/lib/live/mount/overlay/home/amnesia/.gnupg/video.mp4" exists
-    And I restart monitoring the AppArmor log of "/usr/bin/totem"
-    When I try to open "/lib/live/mount/overlay/home/amnesia/.gnupg/video.mp4" with Totem
-    Then I see "TotemUnableToOpen.png" after at most 10 seconds
-    And AppArmor has denied "/usr/bin/totem" from opening "/lib/live/mount/overlay/home/amnesia/.gnupg/video.mp4"
-    Given I close Totem
-    And the file "/live/overlay/home/amnesia/.gnupg/video.mp4" exists
-    And I restart monitoring the AppArmor log of "/usr/bin/totem"
-    When I try to open "/live/overlay/home/amnesia/.gnupg/video.mp4" with Totem
-    Then I see "TotemUnableToOpen.png" after at most 10 seconds
-    # Due to our AppArmor aliases, /live/overlay will be treated
-    # as /lib/live/mount/overlay.
-    And AppArmor has denied "/usr/bin/totem" from opening "/lib/live/mount/overlay/home/amnesia/.gnupg/video.mp4"
-    Given I close Totem
-    And I copy "/home/amnesia/video.mp4" to "/home/amnesia/.purple/otr.private_key" as user "amnesia"
+    And I copy the sample videos to "/home/amnesia/.purple/otr.private_key" as user "amnesia"
+    And the file "/home/amnesia/.purple/otr.private_key" exists
     And I restart monitoring the AppArmor log of "/usr/bin/totem"
     When I try to open "/home/amnesia/.purple/otr.private_key" with Totem
     Then I see "TotemUnableToOpen.png" after at most 10 seconds

commit a8991e46acb444d3808ff582cf189fcfb1705ca9

Repair test case that I've broken earlier on this branch.
I've broken it in c6541323cb70f7c2d1af77bb407bef0d3d3e554a.

This:

   I copy the sample videos to "/home/amnesia/.purple/otr.private_key" as user "amnesia"

… can't work as the destination for this step must be a directory.
So obviously it fails:

   cp: cannot create regular file '/home/amnesia/.purple/otr.private_key/video.mp4': No such file or directory

features/totem.feature
@@ -25,7 +25,7 @@ Feature: Using Totem
     Then I see "TotemUnableToOpen.png" after at most 10 seconds
     And AppArmor has denied "/usr/bin/totem" from opening "/home/amnesia/.gnupg/video.mp4"
     Given I close Totem
-    And I copy the sample videos to "/home/amnesia/.purple/otr.private_key" as user "amnesia"
+    And I copy "/home/amnesia/video.mp4" to "/home/amnesia/.purple/otr.private_key" as user "amnesia"
     And the file "/home/amnesia/.purple/otr.private_key" exists
     And I restart monitoring the AppArmor log of "/usr/bin/totem"
     When I try to open "/home/amnesia/.purple/otr.private_key" with Totem