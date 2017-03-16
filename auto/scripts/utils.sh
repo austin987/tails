b39a8621f3c78b706dffd1bfd90137a21f929020
anonym <anonym@riseup.net>
2017-03-16 22:44:31 +0100

    auto/scripts/utils.sh: don't reinvent the wheel.

--- auto/scripts/utils.sh
+++ auto/scripts/utils.sh
@@ -70,7 +70,7 @@ on_base_branch() {
 
 # Returns the top commit ref of the base branch
 git_base_branch_head() {
-	git rev-parse "$(base_branch)"
+	git_commit_from_ref "$(base_branch)"
 }
 
 branch_name_to_suite() {
