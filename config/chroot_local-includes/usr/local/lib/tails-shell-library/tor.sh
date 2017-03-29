866b8c5e367a2111d0d81156257fa110cf02ba19
anonym <anonym@riseup.net>
2017-03-29 17:15:00 +0200

    Get tor's bootstrap progress via GETINFO instead of log grep:ing.
    I suspect the "Potential Tor bug" doesn't exist any more, if ever, so
    let's see if we can get rid of this crap.

--- config/chroot_local-includes/usr/local/lib/tails-shell-library/tor.sh
+++ config/chroot_local-includes/usr/local/lib/tails-shell-library/tor.sh
@@ -29,7 +29,6 @@ tor_control_send() {
 	    socat - UNIX-CONNECT:$(get_tor_control_socket_path) | tr -d "\r"
 }
 
-# This function may be dangerous to use. See "Potential Tor bug" below.
 # Only handles GETINFO keys with single-line answers
 tor_control_getinfo() {
 	tor_control_send "GETINFO ${1}" | \
@@ -46,22 +45,11 @@ tor_control_setconf() {
 }
 
 tor_bootstrap_progress() {
-	RES=$(grep -o "\[notice\] Bootstrapped [[:digit:]]\+%:" ${TOR_LOG} | \
-	    tail -n1 | sed "s|\[notice\] Bootstrapped \([[:digit:]]\+\)%:|\1|")
-	if [ -z "${RES:-}" ] ; then
-		RES=0
-	fi
-	echo -n "$RES"
+       local res
+       res=$(tor_control_getinfo status/bootstrap-phase | \
+                    sed 's/^.* BOOTSTRAP PROGRESS=\([[:digit:]]\+\) .*$/\1/')
+       echo ${res:-0}
 }
-
-# Potential Tor bug: it seems like using this version makes Tor get
-# stuck at "Bootstrapped 5%" quite often. Is Tor sensitive to opening
-# control ports and/or issuing "getinfo status/bootstrap-phase" during
-# early bootstrap? Because of this we fallback to greping the log.
-#tor_bootstrap_progress() {
-#	tor_control_getinfo status/bootstrap-phase | \
-#	    sed 's/^.* BOOTSTRAP PROGRESS=\([[:digit:]]\+\) .*$/\1/'
-#}
 
 tor_is_working() {
 	[ -e $TOR_DESCRIPTORS ] || [ -e $NEW_TOR_DESCRIPTORS ] || return 1
--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -70,7 +70,6 @@ def post_snapshot_restore_hook
   if $vm.has_network?
     if $vm.execute("systemctl --quiet is-active tor@default.service").success?
       $vm.execute("systemctl stop tor@default.service")
-      $vm.execute("rm -f /var/log/tor/log")
       $vm.execute("systemctl --no-block restart tails-tor-has-bootstrapped.target")
       $vm.host_to_guest_time_sync
       $vm.execute("systemctl start tor@default.service")
