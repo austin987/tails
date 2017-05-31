e43bd8090a84d8107538983ac86f9378d24cbf55
anonym <anonym@riseup.net>
2017-05-31 14:20:42 +0200

    Unsafe Browser: disable searching in the address bar.
    It can result in leaking hosts and credentials to the default search
    engine operator.
    
    Will-fix: #12540

--- config/chroot_local-includes/usr/share/tails/chroot-browsers/unsafe-browser/prefs.js
+++ config/chroot_local-includes/usr/share/tails/chroot-browsers/unsafe-browser/prefs.js
@@ -20,3 +20,9 @@ pref("browser.download.panel.shown", true);
 // Web pages does not render when e10s is enabled, so we have to
 // disable it. Note that the "user_"-prefix is required.
 user_pref("browser.tabs.remote.autostart.2", false);
+
+// Disable searching from the URL bar. Mistyping e.g. the IP address
+// to your router or some LAN resource could leak to the default
+// search engine (this could include credentials, e.g. if something
+// like the following is mistyped: ftp://user:password@host).
+pref("keyword.enabled", false);
