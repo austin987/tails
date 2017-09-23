da1446d5c6422150c229b06d597d88193f28e5ef
sajolida <sajolida@pimienta.org>
2017-09-23 17:35:36 +0000

    Use single quote consistently

--- wiki/src/install/inc/js/dave_2.js
+++ wiki/src/install/inc/js/dave_2.js
@@ -1,24 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
 
   function opaque(elm) {
-      elm.style.opacity = "1.0";
+      elm.style.opacity = '1.0';
       var siblings = elm.querySelectorAll('a');
       for (let i = 0; i < siblings.length; i++) {
-        siblings[i].classList.remove("disabled");
+        siblings[i].classList.remove('disabled');
       }
   }
 
   function transparent(elm) {
-      elm.style.opacity = "0.3";
+      elm.style.opacity = '0.3';
       var siblings = elm.querySelectorAll('a');
       for (let i = 0; i < siblings.length; i++) {
-        siblings[i].classList.add("disabled");
+        siblings[i].classList.add('disabled');
       }
   }
 
   function toggleOpacity(elm, mode) {
     for(var i = 0; i < elm.length; i++) {
-      if(mode == "opaque") {
+      if(mode == 'opaque') {
         opaque(elm[i]);
       } else {
         transparent(elm[i]);
@@ -27,16 +27,16 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   function hide(elm) {
-      elm.style.display = "none";
+      elm.style.display = 'none';
   }
 
   function show(elm) {
-      elm.style.display = "block";
+      elm.style.display = 'block';
   }
 
   function toggleDisplay(elm, mode) {
     for(var i = 0; i < elm.length; i++) {
-      if(mode == "hide") {
+      if(mode == 'hide') {
         hide(elm[i]);
       } else {
         show(elm[i]);
@@ -45,14 +45,14 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   // Show initial screen for supported browser
-  toggleDisplay(document.getElementsByClassName('no-js'), "hide");
-  toggleDisplay(document.getElementsByClassName('supported-browser'), "show");
-  transparent(document.getElementById('step-verify-direct'), "transparent");
-  transparent(document.getElementById('step-continue-direct'), "transparent");
-  transparent(document.getElementById('step-verify-bittorrent'), "transparent");
+  toggleDisplay(document.getElementsByClassName('no-js'), 'hide');
+  toggleDisplay(document.getElementsByClassName('supported-browser'), 'show');
+  transparent(document.getElementById('step-verify-direct'), 'transparent');
+  transparent(document.getElementById('step-continue-direct'), 'transparent');
+  transparent(document.getElementById('step-verify-bittorrent'), 'transparent');
 
   // Display "Verify with your browser" when "Direct download is clicked"
-  document.getElementById("direct-download").onclick = function() {
+  document.getElementById('direct-download').onclick = function() {
     opaque(document.getElementById('step-verify-direct'));
     hide(document.getElementById('skip-download-direct'));
     show(document.getElementById('skip-verification-direct'));
