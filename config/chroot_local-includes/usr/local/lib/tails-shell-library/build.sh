4d6ea3b95a3349fd577e7017066df7fcf9ce2687
anonym <anonym@riseup.net>
2017-10-06 14:06:41 +0200

    Introduce a better mechanism for installing build hook dependencies.
    That will auto-clean up if necessary! Yay!

--- config/chroot_local-includes/usr/local/lib/tails-shell-library/build.sh
+++ config/chroot_local-includes/usr/local/lib/tails-shell-library/build.sh
@@ -9,14 +9,28 @@ strip_nondeterminism_wrapper() {
     apt-get --yes purge strip-nondeterminism '^libfile-stripnondeterminism-perl'
 }
 
+# Ensure that the packages passed as arguments are installed. If they
+# are installed now, they will be marked as "automatically installed"
+# so the next `apt-get autoremove` action *unless* they are later
+# explicitly installed (or other packages depends on them).
+build_hook_dependency() {
+    # Filter out already installed packages from $@.
+    for p in "${@}"; do
+        shift
+        is_package_installed "${p}" && continue
+        set -- "${@}" "${p}"
+    done
+    [ -z "${@}" ] && return
+    apt-get install --yes "${@}"
+    apt-mark auto "${@}"
+}
+
 install_fake_package() {
     local name version section tmp control_file
     name="${1}"
     version="${2}"
     section="${3:-misc}"
-    if ! is_package_installed equivs; then
-        apt-get install --yes equivs
-    fi
+    build_hook_dependency equivs
     tmp="$(mktemp -d)"
     control_file="${tmp}/${name}_${version}.control"
     cat > "${control_file}" << EOF
