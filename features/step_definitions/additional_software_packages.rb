cd7b260f13182fd41dcf4bbd37f529fd77bef190
anonym <anonym@riseup.net>
2018-01-27 14:27:58 +0100

    Fix indentation.

--- features/step_definitions/additional_software_packages.rb
+++ features/step_definitions/additional_software_packages.rb
@@ -1,14 +1,14 @@
 When /^I configure additional software packages to install "(.+?)"$/ do |package|
-    $vm.execute("echo #{package} > /live/persistence/TailsData_unlocked/live-additional-software.conf")
+  $vm.execute("echo #{package} > /live/persistence/TailsData_unlocked/live-additional-software.conf")
 end
 
 # We have to save the non-onion APT sources in persistence, so that on
 # next boot the additional software packages service has the right APT
 # indexes to install the package we want.
 When /^I add non-onion APT sources to persistence$/ do
-    $vm.execute("install -d -m 755 /live/persistence/TailsData_unlocked/apt-sources.list.d")
-    $vm.execute("echo '/etc/apt/sources.list.d  source=apt-sources.list.d,link' >> /live/persistence/TailsData_unlocked/persistence.conf")
-    $vm.execute("cat /etc/apt/sources.list /etc/apt/sources.list.d/* > /live/persistence/TailsData_unlocked/apt-sources.list.d/non-onion.list")
+  $vm.execute("install -d -m 755 /live/persistence/TailsData_unlocked/apt-sources.list.d")
+  $vm.execute("echo '/etc/apt/sources.list.d  source=apt-sources.list.d,link' >> /live/persistence/TailsData_unlocked/persistence.conf")
+  $vm.execute("cat /etc/apt/sources.list /etc/apt/sources.list.d/* > /live/persistence/TailsData_unlocked/apt-sources.list.d/non-onion.list")
 end
 
 Then /^the additional software package installation service is run$/ do
