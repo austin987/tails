50a0da60f89c47162fe2e2377fbc4ad464452a2c
intrigeri <intrigeri@boum.org>
2018-02-21 08:19:39 +0000

    Test suite: adjust dhclient listening address for Buster.

--- features/support/config.rb
+++ features/support/config.rb
@@ -72,7 +72,7 @@ MISC_FILES_DIR = "#{Dir.pwd}/features/misc_files"
 SERVICES_EXPECTED_ON_ALL_IFACES =
   [
    ["cupsd",    "*", "631"],
-   ["dhclient", "*", "68"]
+   ["dhclient", "0.0.0.0", "68"]
   ]
 # OpenDNS
 SOME_DNS_SERVER = "208.67.222.222"
