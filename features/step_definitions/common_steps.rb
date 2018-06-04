austin987/tails

commit e521594433d773acf4f6db991e72cd2a2dee4be4

Test suite: drop test case about migrating from a Jessie-area persistent volume.
If our code happens to support Tails 2.x → 4.x upgrades without going through
3.x, fine. But let's not spend cycles in our CI to guarantee this.

features/step_definitions/common_steps.rb
@@ -564,19 +564,7 @@ Given /^the package "([^"]+)" is installed$/ do |package|
 end
 
 Given /^I add a ([a-z0-9.]+ |)wired DHCP NetworkManager connection called "([^"]+)"$/ do |version, con_name|
-  if version and version == '2.x'
-    con_content = <<EOF
-[connection]
-id=#{con_name}
-uuid=b04afa94-c3a1-41bf-aa12-1a743d964162
-interface-name=eth0
-type=ethernet
-EOF
-    con_file = "/etc/NetworkManager/system-connections/#{con_name}"
-    $vm.file_overwrite(con_file, con_content)
-    $vm.execute_successfully("chmod 600 '#{con_file}'")
-    $vm.execute_successfully("nmcli connection load '#{con_file}'")
-  elsif version and version == '3.x'
+  if version
     raise "Unsupported version '#{version}'"
   else
     $vm.execute_successfully(

features/persistence.feature
@@ -44,20 +44,6 @@ Feature: Tails persistence
     And the 1st network device has a spoofed MAC address configured
     And no network device leaked the real MAC address
 
-  Scenario: Creating and using a Jessie-area persistent NetworkManager connection
-    Given I have started Tails without network from a USB drive with a persistent partition enabled and logged in
-    And the network is plugged
-    And Tor is ready
-    And I add a 2.x wired DHCP NetworkManager connection called "persistent-con-2.x"
-    And I shutdown Tails and wait for the computer to power off
-    Given I start Tails from USB drive "__internal" with network unplugged and I login with persistence enabled
-    And I capture all network traffic
-    And the network is plugged
-    And Tor is ready
-    And I switch to the "persistent-con-2.x" NetworkManager connection
-    And the 1st network device has a spoofed MAC address configured
-    And no network device leaked the real MAC address
-
   Scenario: Deleting a Tails persistent partition
     Given I have started Tails without network from a USB drive with a persistent partition and stopped at Tails Greeter's login screen
     And I log in to a new session

commit 738be9d772d08565611b08143acd9d4e4e4af7cc

Test suite: fix check for NM connection version.
This fixes a regression I've introduced in
commit:e521594433d773acf4f6db991e72cd2a2dee4be4.

features/step_definitions/common_steps.rb
@@ -564,7 +564,7 @@ Given /^the package "([^"]+)" is installed$/ do |package|
 end
 
 Given /^I add a ([a-z0-9.]+ |)wired DHCP NetworkManager connection called "([^"]+)"$/ do |version, con_name|
-  if version
+  if not version.empty?
     raise "Unsupported version '#{version}'"
   else
     $vm.execute_successfully(