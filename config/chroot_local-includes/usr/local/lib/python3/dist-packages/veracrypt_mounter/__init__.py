7450665e6c6996e369253507a9031034559c89c7
segfault <segfault@riseup.net>
2018-07-05 23:20:33 +0200

    Change veracrypt-mounter translation domain to "tails"

--- config/chroot_local-includes/usr/local/lib/python3/dist-packages/veracrypt_mounter/__init__.py
+++ config/chroot_local-includes/usr/local/lib/python3/dist-packages/veracrypt_mounter/__init__.py
@@ -3,12 +3,10 @@
 import os
 import gettext
 
-from .config import APP_NAME
-
 
 if os.path.exists('po/locale'):
-    translation = gettext.translation(APP_NAME, 'po/locale', fallback=True)
+    translation = gettext.translation("tails", 'po/locale', fallback=True)
 else:
-    translation = gettext.translation(APP_NAME, '/usr/share/locale', fallback=True)
+    translation = gettext.translation("tails", '/usr/share/locale', fallback=True)
 
 _ = translation.gettext
