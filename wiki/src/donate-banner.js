5a2f827ce0a85d0856e33cf12bf7caa087c6e493
sajolida <sajolida@pimienta.org>
2018-10-27 10:25:17 +0000

    Add 'Last updated' info on donation counter

--- wiki/src/donate-banner.js
+++ wiki/src/donate-banner.js
@@ -15,4 +15,13 @@ document.addEventListener('DOMContentLoaded', function() {
     numberOfDays[i].textContent = Math.round((endOfCampaign-now)/24/60/60/1000);
    }
 
+  // Display #counter-last-updated when hover on #counter-last-updated-info
+  document.getElementById('counter-last-updated-info').style.display = "inline-block";
+  document.getElementById('counter-last-updated-info').onmouseover = function() {
+    document.getElementById('counter-last-updated').style.display = "block";
+  }
+  document.getElementById('counter-last-updated-info').onmouseout = function() {
+    document.getElementById('counter-last-updated').style.display = "none";
+  }
+
 });
