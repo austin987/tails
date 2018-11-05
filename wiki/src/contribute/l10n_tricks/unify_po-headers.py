75359bd646d2ab32756b9b5261ed5244d6a3b4cb
Sandro Knauß <hefee@debian.org>
2018-11-05 16:43:21 +0100

    Merge unify_po with check_po.
    As most of the stuff is the same for checking and unifying it make sense
    to have only one script for both tasks.

--- wiki/src/contribute/l10n_tricks/unify_po-headers.py
+++ wiki/src/contribute/l10n_tricks/unify_po-headers.py
@@ -26,9 +26,39 @@ import glob
 import multiprocessing
 import re
 import subprocess
-from typing import List, Pattern
+import sys
+from typing import Dict, List, Pattern
 
 MSGCAT_OPTIONS = ["-w", "79"] # wrap to 79 width
+
+# i18nspector issues, that we accept
+I18NSPECTOR_ACCEPT = [
+        "boilerplate-in-date",
+        "boilerplate-in-initial-comments",
+        "boilerplate-in-language-team",
+        "boilerplate-in-last-translator",
+        "boilerplate-in-project-id-version",
+        "codomain-error-in-plural-forms",
+        "codomain-error-in-unused-plural-forms",
+        "conflict-marker-in-header-entry",
+        "fuzzy-header-entry",
+        "incorrect-plural-forms",
+        "invalid-content-transfer-encoding",
+        "invalid-date",
+        "invalid-language",
+        "invalid-last-translator",
+        "language-team-equal-to-last-translator",
+        "no-language-header-field",
+        "no-package-name-in-project-id-version",
+        "no-plural-forms-header-field",
+        "no-report-msgid-bugs-to-header-field",
+        "no-version-in-project-id-version",
+        "stray-previous-msgid",
+        "unable-to-determine-language",
+        "unknown-poedit-language",
+        "unusual-plural-forms",
+        "unusual-unused-plural-forms",
+        ]
 
 class PoFile:
     def regexKey(self, key: str) -> Pattern:
@@ -47,6 +77,15 @@ class PoFile:
         """magic method for with statement"""
         if type is None and value is None and traceback is None:
             self.write()
+
+    def fixedHeaders(self) -> Dict[str, str]:
+        """@returns: a dict of key,value parts that should be fixed within the po file"""
+        return {"Language": self.lang(),
+                "Content-Type": "text/plain; charset=UTF-8",
+                "Project-Id-Version": "",
+                "Language-Team": "Tails translators <tails-l10n@boum.org>",
+                "Last-Translator": "Tails translators",
+                }
 
     def open(self) -> None:
         """read po file content"""
@@ -84,8 +123,11 @@ class PoFile:
             with open(self.fname, 'w') as f:
                 f.write(self.content)
 
-    def msgcat(self) -> None:
-        """runs msgcat over file, if file is already opened, than only the opened copy is modified"""
+    def msgcat(self, modify=False) -> bool:
+        """runs msgcat over file, only the opened copy is modified.
+        @modify: if True, the file content gets updated, otherwise only checked.
+        @returns: if the content has/needs to be changed
+        """
         cmd = ["msgcat",] + MSGCAT_OPTIONS
         if hasattr(self, "content"):
             cmd.append("-") # use stdin as input
@@ -95,27 +137,57 @@ class PoFile:
                     stdout=subprocess.PIPE,
                     check=True)
             if process.stdout != content:
-                self.content = process.stdout.decode()
-                self.__changed = True
-        else:
-            cmd.extend(["-o", self.fname]) # output to file itself
-            cmd.append(self.fname) # file as input
-            subprocess.check_call(cmd)
+                if modify:
+                    self.content = process.stdout.decode()
+                    self.__changed = True
+                return True
+            else:
+                return False
+
+        raise Exception("please run obj.open() before using this method.")
+
+    def i18nspector(self) -> List[str]:
+        """@returns a list of issues raised by i18nspector removes allowed issues from @I18NINSPECTOR_ALLOWED_ISSUES.
+        """
+        cmd = ["i18nspector", "-l", self.lang(), self.fname]
+        process = subprocess.run(cmd,
+                stdout=subprocess.PIPE,
+                check=True)
+        issues = []
+        for line in process.stdout.decode().strip().split("\n"):
+            severity, fname, issue, *content = line.split(" ")
+            if issue not in I18NSPECTOR_ACCEPT:
+                issues.append(" ".join([severity, issue, *content]))
+
+        return issues
+
+def checkPoFile(fname: str, extended: bool) -> List[str]:
+    """check PO file for issues.
+    @returns: a list of errors, if empty the file is fine.
+    @extended: is used to check the header fields in more detail.
+    """
+    errors = list()
+    with PoFile(fname) as poFile:
+        issues = poFile.i18nspector()
+        if issues:
+            errors.append("i18nspector is not happy:\n\t"+"\n\t".join(issues))
+
+        if poFile.msgcat():
+            errors.append("Not rewrapped to 79 chars.")
+
+        if extended:
+            for key, value in poFile.fixedHeaders().items():
+                if not poFile.check(key, value):
+                    errors.append("{key} is not '{value}'.".format(key=key, value=value))
+
+    return errors
 
 def unifyPoFile(fname: str) -> None:
     """unify PO header and run msgcat for file named fname"""
     with PoFile(fname) as poFile:
-        poFile.unifyKey("Language", poFile.lang())
-        poFile.unifyKey("Content-Type", "text/plain; charset=UTF-8")
-        poFile.unifyKey("Project-Id-Version", "")
-        poFile.unifyKey("Language-Team", "Tails translators <tails-l10n@boum.org>")
-        poFile.unifyKey("Last-Translator", "Tails translators")
-        poFile.msgcat()
-
-def main(files: List[str]) -> None:
-    """unify PO headers for a list of files"""
-    pool = multiprocessing.Pool()
-    list(pool.map(unifyPoFile,files))
+        for key, value in poFile.fixedHeaders().items():
+            poFile.unifyKey(key, value)
+        poFile.msgcat(modify=True)
 
 if __name__ == '__main__':
     import argparse
@@ -123,6 +195,8 @@ if __name__ == '__main__':
     import os.path
 
     parser = argparse.ArgumentParser(description='Unify PO files')
+    parser.add_argument('--modify', dest='modify', action='store_true',  help='Modify the PO headers, otherwise only check is done.')
+    parser.add_argument('--check-extended', dest='extended', action='store_true',  help='Do extended checks of PO headers.')
     parser.add_argument('--lang', dest='lang', help='all files of a specific language.')
     parser.add_argument('--cached', dest='cached', action='store_true', help='all git staged PO files.')
     parser.add_argument('files', metavar='file', type=str, nargs='*',
@@ -130,7 +204,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if args.lang:
-        args.files += glob.glob("**/*.{lang}.po".format(lang=args.lang))
+        args.files += glob.glob("**/*.{lang}.po".format(lang=args.lang), recursive=True)
 
     if args.cached:
         # get top level directory of the current git repository
@@ -145,6 +219,28 @@ if __name__ == '__main__':
         args.files += [os.path.join(toplevel,f) for f in output.decode().split("\n") if f.endswith(".po")]
 
     if not args.files:
-        print("WARNING: no file to unify:( You may want to add files to operate on. See --help for further information.")
+        print("WARNING: no file to process :( You may want to add files to operate on. See --help for further information.")
 
-    main(args.files)
+    e = None
+    try:
+        if args.modify:
+            # unify PO headers for a list of files
+            pool = multiprocessing.Pool()
+            list(pool.map(unifyPoFile, args.files))
+        else:
+            fine = True
+            # check only the headers
+            for fname in args.files:
+                issues = checkPoFile(fname, extended=args.extended)
+                if issues:
+                    fine = False
+                    issues = [i.replace("\n","\n\t") for i in issues] # indent subissues
+                    print("Issues with {fname}:\n\t{issues}".format(fname=fname, issues="\n\t".join(issues)))
+
+            if not fine:
+                sys.exit("checked files are not clean.")
+    except FileNotFoundError as err:
+        if err.filename in ("i18nspector","msgcat"):
+            sys.exit("{fname}: command not found\nYou need to install {fname} first. See /contribute/l10n_tricks.".format(fname=err.filename))
+        else:
+            raise
