austin987/tails

commit 710d832fddfe42b79cafa8c65d12b115a0cdb0bd

Test suite: Turn some steps definition variables into non-capturing regexp.
Refs: #14596

features/step_definitions/additional_software_packages.rb
@@ -59,7 +59,7 @@ Then /^"([^"]*)" is not in the list of Additional Software$/ do |package|
   $vm.execute("grep \"#{package}\" #{ASP_CONF}").stdout.empty?
 end
 
-When /^I (refuse|accept) (adding|removing) "([^"]*)" (to|from) Additional Software$/  do |decision, action, package, destination|
+When /^I (refuse|accept) (adding|removing) "([^"]*)" (?:to|from) Additional Software$/  do |decision, action, package, destination|
   gnome_shell = Dogtail::Application.new('gnome-shell')
   case action
   when "adding"

features/step_definitions/common_steps.rb
@@ -138,7 +138,7 @@ Given /^I start Tails( from DVD)?( with network unplugged)?( and genuine APT sou
   end
 end
 
-Given /^I start Tails from (.+?) drive "(.+?)"( with network unplugged)?( and I login( with persistence enabled)?( (and|with) an administration password)?)?$/ do |drive_type, drive_name, network_unplugged, do_login, persistence_on, and_or_with, admin_password|
+Given /^I start Tails from (.+?) drive "(.+?)"( with network unplugged)?( and I login( with persistence enabled)?( (?:and|with) an administration password)?)?$/ do |drive_type, drive_name, network_unplugged, do_login, persistence_on, and_or_with, admin_password|
   step "the computer is set to boot from #{drive_type} drive \"#{drive_name}\""
   if network_unplugged
     step "the network is unplugged"

commit f8dce38cd913ce771c1aea1c76408ac9671aef7a

Test suite: Fix non-caprturing regexp handling in some steps.
Bug introduced with 710d832fddfe42b79cafa8c65d12b115a0cdb0bd

Refs: #14596

features/step_definitions/additional_software_packages.rb
@@ -63,7 +63,7 @@ Then /^"([^"]*)" is not in the list of Additional Software$/ do |package|
   end
 end
 
-When /^I (refuse|accept) (adding|removing) "([^"]*)" (?:to|from) Additional Software$/  do |decision, action, package, destination|
+When /^I (refuse|accept) (adding|removing) "([^"]*)" (?:to|from) Additional Software$/  do |decision, action, package|
   gnome_shell = Dogtail::Application.new('gnome-shell')
   case action
   when "adding"

features/step_definitions/common_steps.rb
@@ -138,7 +138,7 @@ Given /^I start Tails( from DVD)?( with network unplugged)?( and genuine APT sou
   end
 end
 
-Given /^I start Tails from (.+?) drive "(.+?)"( with network unplugged)?( and I login( with persistence enabled)?( (?:and|with) an administration password)?)?$/ do |drive_type, drive_name, network_unplugged, do_login, persistence_on, and_or_with, admin_password|
+Given /^I start Tails from (.+?) drive "(.+?)"( with network unplugged)?( and I login( with persistence enabled)?( (?:and|with) an administration password)?)?$/ do |drive_type, drive_name, network_unplugged, do_login, persistence_on, admin_password|
   step "the computer is set to boot from #{drive_type} drive \"#{drive_name}\""
   if network_unplugged
     step "the network is unplugged"