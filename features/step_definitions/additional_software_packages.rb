1237235f5af0b51e867107f2f86d21f1d9f3d687
bertagaz <bertagaz@ptitcanardnoir.org>
2018-11-20 12:30:06 +0100

    Test suite: Ensure the ASP package files removal is effective before going in on.
    In some Jenkins run the cowsay deb file was not really removed and
    installation succeeded in the last scenario. Let's check the file are
    gone before trying to fail installing them.
    
    Refs: #14596

--- features/step_definitions/additional_software_packages.rb
+++ features/step_definitions/additional_software_packages.rb
@@ -109,7 +109,10 @@ EOF
 end
 
 When /^I remove the "([^"]*)" deb files from the APT cache$/  do |package|
-  $vm.execute("rm /live/persistence/TailsData_unlocked/apt/cache/#{package}_*.deb")
+  $vm.execute("rm /live/persistence/TailsData_unlocked/apt/cache/#{package}_*.deb && sync")
+  try_for(60) do
+    $vm.execute("ls /live/persistence/TailsData_unlocked/apt/cache/#{package}_*.deb").failure?
+  end
 end
 
 Then /^I can open the Additional Software documentation from the notification link$/  do
