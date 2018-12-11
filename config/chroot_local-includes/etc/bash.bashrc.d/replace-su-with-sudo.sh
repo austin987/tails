3446570d7cbddfe45f39d4089db0c92d801a4f34
m3hm00d <f.m3hm00d@gmail.com>
2018-12-11 21:45:26 +0500

    Add internationalization support for 'replace-su-with-sudo.sh'
    Appropriate changes made to 'POTFILES.in', 'refresh_translations',
    and 'replace-su-with-sudo.sh'.

--- config/chroot_local-includes/etc/bash.bashrc.d/replace-su-with-sudo.sh
+++ config/chroot_local-includes/etc/bash.bashrc.d/replace-su-with-sudo.sh
@@ -3,12 +3,21 @@
 # Replace su by a message to use sudo
 # https://redmine.tails.boum.org/code/issues/15583
 
+# Get LANG
+. /etc/default/locale
+export LANG
+
+# Initialize gettext support
+. gettext.sh
+TEXTDOMAIN="tails"
+export TEXTDOMAIN
+
 [ $USER == root ] && return
 
 su (){
     tails_is_password_set.py
     if [ $? -eq 0 ]; then
-        echo su is disabled. Please use sudo instead.
+        "`gettext \"su is disabled. Please use sudo instead.\"`"
     else
         cat /usr/share/tails-greeter/no-password-lecture.txt
     fi
