dcfd96b9024f3bc1b9d6d23c2bbf5cd3b3fd8e39
Sandro Knauß <hefee@debian.org>
2019-04-05 09:13:53 +0200

    Disable man-db.timer on Buster (refs: #16631)

--- config/chroot_local-hooks/52-update-rc.d
+++ config/chroot_local-hooks/52-update-rc.d
@@ -70,3 +70,6 @@ systemctl mask pppd-dns.service
 
 # Conflicts with our custom shutdown procedure
 systemctl mask live-tools.service
+
+# "Daily man-db regeneration" is not needed in Tails (#16631)
+systemctl mask man-db.timer
