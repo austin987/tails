d37dd57573b3470d1c702fef30f4914a99fbadcb
sajolida <sajolida@pimienta.org>
2019-04-29 17:55:36 +0000

    Use a more explicit naming

--- wiki/src/home.js
+++ wiki/src/home.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
   var randomMessages = document.getElementsByClassName('random-message');
   for (let i = 0; i < randomMessages.length; i++) {
     var message = randomMessages[i]
-    if(Math.floor(message.dataset.n * Math.random()) == 0) {
+    if(Math.floor(1/message.dataset.displayProbability * Math.random()) == 0) {
       message.style.display = "block";
       if(message.id == "donate") {
         document.documentElement.dataset.hideSidebarDonate = "true";
