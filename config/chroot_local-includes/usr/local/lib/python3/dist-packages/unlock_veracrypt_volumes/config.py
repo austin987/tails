4defaadda9d177745d09b55aef99cc43cd54af9d
intrigeri <intrigeri@boum.org>
2019-05-18 13:34:09 +0000

    Unlock VeraCrypt Volumes: set correct translation domain (refs: #16602)

--- config/chroot_local-includes/usr/local/lib/python3/dist-packages/unlock_veracrypt_volumes/config.py
+++ config/chroot_local-includes/usr/local/lib/python3/dist-packages/unlock_veracrypt_volumes/config.py
@@ -5,3 +5,4 @@ DATA_DIR = "/usr/share/%s/" % APP_NAME
 UI_DIR = path.join(DATA_DIR, "ui")
 MAIN_UI_FILE = path.join(UI_DIR, "main.ui")
 VOLUME_UI_FILE = path.join(UI_DIR, "volume.ui")
+TRANSLATION_DOMAIN = "tails"
