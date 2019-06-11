898fbbd6a5f8d10058c128d996bb6571c351fe90
anonym <anonym@riseup.net>
2019-06-11 13:48:35 +0200

    Remove useless, commented code.

--- features/step_definitions/dhcp.rb
+++ features/step_definitions/dhcp.rb
@@ -3,8 +3,6 @@ Then /^the hostname should not have been leaked on the network$/ do
     hostnames = ["amnesia", $vm.execute("hostname").stdout.chomp]
     packets = PacketFu::PcapFile.new.file_to_array(filename: @sniffer.pcap_file)
     packets.each do |p|
-      # if PacketFu::TCPPacket.can_parse?(p)
-      #   ipv4_tcp_packets << PacketFu::TCPPacket.parse(p)
       if PacketFu::IPPacket.can_parse?(p)
         payload = PacketFu::IPPacket.parse(p).payload
       elsif PacketFu::IPv6Packet.can_parse?(p)
