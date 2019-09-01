f13ea3fde5ba188e42ffdb280d83445dae9437ed
intrigeri <intrigeri@boum.org>
2019-09-01 09:14:59 +0000

    Test suite: optimize performance of the "all notifications are disappeared" (refs: #17012)
    The robustness fix in the previous commit makes this step quite slow in the most
    common case — that is: a notification is displayed — because we'll have to wait
    for Dogtail to give up waiting on the "No Notifications" label, which is nowhere
    to be seen.
    
    Let's optimize for the case where there is at least a notification, such as the
    virtual machine warning, and look for the "Clear All" [notifications]
    button first.
    
    In passing, finish Dogtail'ifying this step by dropping its remaining
    Sikuli call.

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -445,8 +445,12 @@ Given /^all notifications have disappeared$/ do
   gnome_shell = Dogtail::Application.new('gnome-shell')
   retry_action(10, recovery_proc: Proc.new { @screen.type(Sikuli::Key.ESC) }) do
     @screen.click_point(x, y)
-    unless gnome_shell.child?('No Notifications', roleName: 'label', showingOnly: true)
-      @screen.click('GnomeCloseAllNotificationsButton.png')
+    begin
+      gnome_shell.child('Clear All', roleName: 'push button', showingOnly: true).click
+    rescue
+      # Ignore exceptions: there might be no notification to clear, in
+      # which case there will be a "No Notifications" label instead of
+      # a "Clear All" button.
     end
     gnome_shell.child?('No Notifications', roleName: 'label', showingOnly: true)
   end
