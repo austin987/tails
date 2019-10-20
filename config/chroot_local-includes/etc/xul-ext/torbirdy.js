b53b55c599be81ccb8b1bd7b836c1c558caf3ac9
intrigeri <intrigeri@boum.org>
2019-10-20 15:09:38 +0000

    Drop broken Torbirdy pref to tweak the Enigmail keyserver (refs: #12689)
    This pref is overriden by Torbirdy. Given it's not clear for how long we'll be
    able to ship Torbirdy, I won't bother investigating, so for now Enigmail will
    still use the old-school keyserver Onion service pool.
    
    Partially reverts dbfbfa7b11857c0adba9b3c6cd38da3a26e2228b.

--- config/chroot_local-includes/etc/xul-ext/torbirdy.js
+++ config/chroot_local-includes/etc/xul-ext/torbirdy.js
@@ -4,4 +4,3 @@
 
 pref("extensions.torbirdy.emailwizard", true);
 pref("extensions.torbirdy.gpg_already_torified", true);
-pref("extensions.torbirdy.custom.extensions.enigmail.keyserver", "hkp://zkaan2xfbuxia2wpf7ofnkbz6r5zdbbvxbunvp5g2iebopbfc4iqmbad.onion");
