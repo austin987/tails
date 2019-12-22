9d600e6a0bae23fc6c45d1f421ba23f4b69fa6a7
intrigeri <intrigeri@boum.org>
2019-12-22 12:21:25 +0000

    Import perl5lib (refs: #7036)
    From branch feature/15281-single-squashfs-diff at commit
    0bfdd05cc094a08d60f07034afcb71344ac4ec14.

---
+++ config/chroot_local-includes/usr/src/perl5lib/lib/Tails.pm
@@ -0,0 +1,21 @@
+=head1 NAME
+
+Tails - Tails Perl library
+
+=head1 VERSION
+
+Version 
+
+=cut
+
+package Tails;
+
+use 5.10.1;
+use strictures 2;
+use autodie qw(:all);
+
+use namespace::clean;
+
+our $VERSION = '2.0.3';
+
+1;
