c04c13edc4446b63ca97393ca7b71c8ef2959cf1
intrigeri <intrigeri@boum.org>
2019-12-27 10:10:38 +0000

    Test suite: don't call "close" method that hangs forever (refs: #14948)
    When proc.close is called once the scenario has completed, for some reason it
    hangs forever. I don't remember seeing this specific child process left
    as a zombie after the test suite completed, and on #14948 we did not
    manage to reach a clear understanding of why calling proc.close
    would help, so let's remove it.
    
    This reverts 5e7ff8e9e644374bea8120096706855bc3c1b5b2 and part
    of cc86a75fe6fdbb5ac54714d62f4b8dfe5831eb80.

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -838,7 +838,7 @@ EOF
     Process.kill(0, proc.pid) == 1
   end
 
-  add_after_scenario_hook { proc.close; Process.kill("TERM", proc.pid) }
+  add_after_scenario_hook { Process.kill("TERM", proc.pid) }
 
   # It seems necessary to actually check that the LAN server is
   # serving, possibly because it isn't doing so reliably when setting
