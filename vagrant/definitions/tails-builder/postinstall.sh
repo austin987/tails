5e30d85d0e41790e6f71b8b0d05efced92fe2aa1
Johan Blåbäck <johan_bluecreek@riseup.net>
2020-01-09 14:59:46 +0100

    syslinux is not used in tails builder anymore

--- vagrant/definitions/tails-builder/postinstall.sh
+++ vagrant/definitions/tails-builder/postinstall.sh
@@ -115,9 +115,6 @@ apt-get -y install \
         python3-gi \
         rsync \
         ruby \
-        syslinux \
-        syslinux-common \
-        syslinux-utils \
         time \
         udisks2 \
         whois
