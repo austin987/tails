austin987/tails

commit ccb37c48150bff1b1a1f45479bb78540979c9c0c

Test suite: remove dead code

features/step_definitions/common_steps.rb
@@ -194,7 +194,7 @@ def boot_menu_cmdline_image
   end
 end
 
-Given /^Tails is at the boot menu's cmdline( after rebooting)?$/ do |reboot|
+Given /^Tails is at the boot menu's cmdline$/ do
   boot_timeout = 3*60
   # Simply looking for the boot splash image is not robust; sometimes
   # sikuli is not fast enough to see it. Here we hope that spamming
@@ -237,8 +237,8 @@ Given /^Tails is at the boot menu's cmdline( after rebooting)?$/ do |reboot|
   end
 end
 
-Given /^the computer (re)?boots Tails( with genuine APT sources)?$/ do |reboot, keep_apt_sources|
-  step "Tails is at the boot menu's cmdline" + (reboot ? ' after rebooting' : '')
+Given /^the computer boots Tails( with genuine APT sources)?$/ do |keep_apt_sources|
+  step "Tails is at the boot menu's cmdline"
   @screen.type(" autotest_never_use_this_option blacklist=psmouse #{@boot_options}" +
                Sikuli::Key.ENTER)
   @screen.wait('TailsGreeter.png', 5*60)

features/step_definitions/erase_memory.rb
@@ -183,10 +183,6 @@ Then /^I find very few patterns in the guest's memory$/ do
          "pattern, but less than #{"%.3f" % (max_coverage*100)}% was expected")
 end
 
-When /^I stop the boot at the bootloader menu$/ do
-  step "Tails is at the boot menu's cmdline"
-end
-
 When /^I wait for Tails to finish wiping the memory$/ do
   @screen.wait("MemoryWipeCompleted.png", 90)
 end

commit cb20740cf4ba1adf4d4f699a03537cc8689701d0

Test suite: bring back "the computer reboots Tails" alternate phrasing
Fixes regression introduced by ccb37c48150bff1b1a1f45479bb78540979c9c0c,
where I removed the special casing of rebooting (because in practice
we run the same code as when we're freshly booting) but missed the fact
that the "the computer reboots Tails" alternate phrasing is used
in a couple places.

features/step_definitions/common_steps.rb
@@ -256,7 +256,7 @@ Given /^Tails is at the boot menu's cmdline$/ do
   @screen.wait(boot_menu_cmdline_image, 5)
 end
 
-Given /^the computer boots Tails( with genuine APT sources)?$/ do |keep_apt_sources|
+Given /^the computer (?:re?)boots Tails( with genuine APT sources)?$/ do |keep_apt_sources|
   step "Tails is at the boot menu's cmdline"
   boot_key = @os_loader == 'UEFI' ? Sikuli::Key.F10 : Sikuli::Key.ENTER
   @screen.type(' autotest_never_use_this_option' \