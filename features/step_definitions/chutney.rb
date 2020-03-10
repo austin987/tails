311db98466e5e514c62711bffdd47879414ba734
anonym <anonym@riseup.net>
2020-03-10 13:41:31 +0100

    Test suite: drop extraneous / from end of CHUTNEY_DATA_DIR path.
    Soon we'll do a pkill which will include
    "#{env['CHUTNEY_DATA_DIR']}/nodes" resulting in a double slash, which
    won't work, but could be fixed with the uglier
    "#{env['CHUTNEY_DATA_DIR']}nodes", which I rather avoid.

--- features/step_definitions/chutney.rb
+++ features/step_definitions/chutney.rb
@@ -16,7 +16,7 @@ def ensure_chutney_is_running
     network_definition = "#{GIT_DIR}/features/chutney/test-network"
     env = {
       'CHUTNEY_LISTEN_ADDRESS' => chutney_listen_address,
-      'CHUTNEY_DATA_DIR' => "#{$config['TMPDIR']}/chutney-data/",
+      'CHUTNEY_DATA_DIR' => "#{$config['TMPDIR']}/chutney-data",
       # The default value (60s) is too short for "chutney wait_for_bootstrap"
       # to succeed reliably.
       'CHUTNEY_START_TIME' => '600',
