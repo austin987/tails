austin987/tails

commit e4c8259d35036298de246afc60405701b9f072ee

Build system: enable website caching by default, with a way option to disable it (refs: #17439)

Rakefile
@@ -59,6 +59,8 @@ ENV['ARTIFACTS'] ||= '.'
 
 ENV['APT_SNAPSHOTS_SERIALS'] ||= ''
 
+ENV['TAILS_WEBSITE_CACHE'] = '1'
+
 class CommandError < StandardError
   attr_reader :status, :stderr
 
@@ -247,12 +249,21 @@ task :parse_build_options do
       ENV['TAILS_PROXY_TYPE'] = 'noproxy'
     when 'offline'
       ENV['TAILS_OFFLINE_MODE'] = '1'
-    when 'cachewebsite'
+    when /cachewebsite(?:=([a-z]+))?/
+      value = $1
       if is_release?
         $stderr.puts "Building a release ⇒ ignoring #{opt} build option"
         ENV['TAILS_WEBSITE_CACHE'] = '0'
       else
-        ENV['TAILS_WEBSITE_CACHE'] = '1'
+        value = 'yes' if value.nil?
+        case value
+        when 'yes'
+          ENV['TAILS_WEBSITE_CACHE'] = '1'
+        when 'no'
+          ENV['TAILS_WEBSITE_CACHE'] = '0'
+        else
+          raise "Unsupported value for cachewebsite option: #{value}"
+        end
       end
     # SquashFS compression settings
     when 'fastcomp', 'gzipcomp'

wiki/src/contribute/build.mdwn
@@ -113,7 +113,7 @@ You can customize the build system using two environment variables:
 
    For example, you can speed up the build by setting:
 
-        export TAILS_BUILD_OPTIONS="ram fastcomp cachewebsite"
+        export TAILS_BUILD_OPTIONS="ram fastcomp"
 
    This will force the build to happen in RAM and SquashFS compression
    will be done using faster, though less efficient size-wise, settings.
@@ -202,9 +202,12 @@ affect reproducibility of the ISO image:
  * **rescue**: implies **keeprunning** and will also not clean up the
    build directory, which is useful for investigating build failures.
 
- * **cachewebsite**: enable caching of the built website.
-   The cache is keyed on the input parameters that primarily determine
-   the output of the website build.
+ * **cachewebsite=**:
+   - If set to "yes" (the default, unless building from a tag), enable caching
+     of the built website. The cache is keyed on the input parameters that
+     primarily determine the output of the website build.
+   - If set to "no", (the default when building from a tag), build the website
+     from scratch.
 
 ## HTTP proxy settings
 

commit 04330e7378edf443c379e65c32b0d9572428cb76

Rakefile: always disable website caching when building from a tag (refs: #17513)
e4c8259d35036298de246afc60405701b9f072ee introduced a regression on this front:
we would turn off website caching only when the cachewebsite build option
was specified.

Rakefile
@@ -59,8 +59,6 @@ ENV['ARTIFACTS'] ||= '.'
 
 ENV['APT_SNAPSHOTS_SERIALS'] ||= ''
 
-ENV['TAILS_WEBSITE_CACHE'] = '1'
-
 class CommandError < StandardError
   attr_reader :status, :stderr
 
@@ -205,6 +203,8 @@ def system_cpus
     nil
   end
 end
+
+ENV['TAILS_WEBSITE_CACHE'] = is_release? ? '0' : '1'
 
 task :parse_build_options do
   options = []

wiki/src/contribute/release_process.mdwn
@@ -500,7 +500,6 @@ Build the almost-final images
    - Set `defaultcomp`, so we can more accurately optimize our
      SquashFS file ordering.
    - Do _not_ set `keeprunning` nor `rescue`.
-   - Set `cachewebsite=no` as a workaround for [[!tails_ticket 17513]]
 2. Carefully read the build logs to make sure nothing bad happened.
 3. Keep the resulting build artifacts until the end of this release process.
 4. Record where the manifest of needed packages is stored:
@@ -647,7 +646,6 @@ suite should be ready, so it is time to:
    Do _not_ set `keeprunning` nor `rescue` in `$TAILS_BUILD_OPTIONS`.
    Our build system will apply the correct compression settings automatically
    so don't bother setting it yourself.
-   Set `cachewebsite=no` as a workaround for [[!tails_ticket 17513]]
 
 1. Compare the new build manifest with the one from the previous,
    almost final build: