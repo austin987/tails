f607bb59fe88c07d6018ab47c2e0defc71a2f80a
intrigeri <intrigeri@boum.org>
2020-04-05 07:53:55 +0000

    Drop unnecessary links to issues

--- config/chroot_local-includes/etc/bash.bashrc.d/replace-su-with-sudo.sh
+++ config/chroot_local-includes/etc/bash.bashrc.d/replace-su-with-sudo.sh
@@ -9,8 +9,6 @@
 # This script introduces 'su' function for non-root users. The 'su'
 # function executes '/usr/local/bin/replace-su-with-sudo', which asks
 # them to use 'sudo' instead of 'su'.
-#
-# https://redmine.tails.boum.org/code/issues/15583
 
 # Get LIVE_USERNAME
 . /etc/live/config.d/username.conf
