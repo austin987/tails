7e77b39bf611a0e92bacf916e501c969fb31617a
intrigeri <intrigeri@boum.org>
2020-04-21 19:06:58 +0200

    Rubocop: fix Style/UnlessElse offenses
    Changes generated with:
    
    `rubocop --only Style/UnlessElse --auto-correct`

--- features/step_definitions/ssh.rb
+++ features/step_definitions/ssh.rb
@@ -41,13 +41,13 @@ end
 Given /^I have the SSH key pair for an? (Git|SSH|SFTP) (?:repository|server)( on the LAN)?$/ do |server_type, lan|
   $vm.execute_successfully("install -m 0700 -d '/home/#{LIVE_USER}/.ssh/'",
                            :user => LIVE_USER)
-  unless server_type == 'Git' || lan
+  if server_type == 'Git' || lan
+    secret_key = $config["Unsafe_SSH_private_key"]
+    public_key = $config["Unsafe_SSH_public_key"]
+  else
     read_and_validate_ssh_config server_type
     secret_key = $config[server_type]["private_key"]
     public_key = $config[server_type]["public_key"]
-  else
-    secret_key = $config["Unsafe_SSH_private_key"]
-    public_key = $config["Unsafe_SSH_public_key"]
   end
 
   $vm.execute_successfully("echo '#{secret_key}' > '/home/#{LIVE_USER}/.ssh/id_rsa'",
