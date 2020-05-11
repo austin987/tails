1f2873d743c1099ff061a1365991ca85b485ba86
intrigeri <intrigeri@boum.org>
2020-05-11 18:32:30 +0000

    Test suite: use simpler and user-facing terminology and phrasing

--- features/step_definitions/root_access_control.rb
+++ features/step_definitions/root_access_control.rb
@@ -1,16 +1,18 @@
-Then /^I should be able to run administration commands as the live user$/ do
+Then /^I can run a command as root with sudo$/ do
   stdout = $vm.execute("echo #{@sudo_password} | sudo -S whoami",
                        user: LIVE_USER).stdout
   actual_user = stdout.sub(/^\[sudo\] password for #{LIVE_USER}: /, '').chomp
   assert_equal('root', actual_user, 'Could not use sudo')
 end
 
-Then /^I should not be able to run administration commands as the live user with the "([^"]*)" password$/ do |password|
-  stderr = $vm.execute("echo #{password} | sudo -S whoami",
-                       user: LIVE_USER).stderr
-  sudo_failed = stderr.include?('The administration password is disabled') \
-                || stderr.include?('is not allowed to execute')
-  assert(sudo_failed, 'The administration password is not disabled:' + stderr)
+Then /^I cannot run a command as root with sudo and the standard passwords$/ do
+  ['', 'live', 'amnesia'].each do |password|
+    stderr = $vm.execute("echo #{password} | sudo -S whoami",
+                         user: LIVE_USER).stderr
+    sudo_failed = stderr.include?('The administration password is disabled') \
+                  || stderr.include?('is not allowed to execute')
+    assert(sudo_failed, 'The administration password is not disabled:' + stderr)
+  end
 end
 
 When /^running a command as root with pkexec requires PolicyKit administrator privileges$/ do
@@ -25,7 +27,7 @@ When /^running a command as root with pkexec requires PolicyKit administrator pr
          "Expected 'auth_admin' for 'active':\n#{action_details}")
 end
 
-Then /^I should be able to run a command as root with pkexec$/ do
+Then /^I can run a command as root with pkexec$/ do
   step 'I run "pkexec touch /root/pkexec-test" in GNOME Terminal'
   step 'I enter the sudo password in the pkexec prompt'
   try_for(10, msg: 'The /root/pkexec-test file was not created.') do
@@ -33,7 +35,7 @@ Then /^I should be able to run a command as root with pkexec$/ do
   end
 end
 
-Then /^I should not be able to run a command as root with pkexec and the standard passwords$/ do
+Then /^I cannot run a command as root with pkexec and the standard passwords$/ do
   step 'I run "pkexec touch /root/pkexec-test" in GNOME Terminal'
   ['', 'live', 'amnesia'].each do |password|
     deal_with_polkit_prompt(password, expect_success: false)
