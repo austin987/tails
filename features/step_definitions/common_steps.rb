b6a1570185abefe75533afe1029eedfd81bb7f62
intrigeri <intrigeri@boum.org>
2020-05-12 06:29:59 +0000

    Test suite: call post_vm_start_hook at a time that makes more sense
    Previously we would call that function immediately after starting the VM, but
    that function does a mouse click, which makes no sense until we have a display
    that supports clicking. This became obvious since I did
    1881d05af8acb17addebe9af891a2e46c05603db, which now makes post_vm_start_hook
    wait for the mouse cursor before exiting, which virtually guarantees we'll miss
    the boot menu.
    
    So, instead of calling post_vm_start_hook immediately after starting
    the VM, do it after the Greeter is visible and the remote shell is up,
    just like we do in post_snapshot_restore_hook already.

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -126,7 +126,6 @@ When /^I start the computer$/ do
   assert(!$vm.running?,
          'Trying to start a VM that is already running')
   $vm.start
-  post_vm_start_hook
 end
 
 Given /^I start Tails( from DVD)?( with network unplugged)?( and genuine APT sources)?( and I login)?$/ do |dvd_boot, network_unplugged, keep_apt_sources, do_login|
@@ -328,6 +327,7 @@ Given /^the computer (?:re)?boots Tails( with genuine APT sources)?$/ do |keep_a
     @screen.find('TailsGreeter.png')
   end
   $vm.wait_until_remote_shell_is_up
+  post_vm_start_hook
   step 'I configure Tails to use a simulated Tor network'
   # This is required to use APT in the test suite as explained in
   # commit e2510fae79870ff724d190677ff3b228b2bf7eac
