bbbc6f16348ad7978f10d3d233c118e920934e6b
sajolida <sajolida@pimienta.org>
2020-07-18 03:17:02 +0000

    Prevent the survey and the donate message to appear at the same time

--- wiki/src/home.js
+++ wiki/src/home.js
@@ -6,7 +6,8 @@ document.addEventListener('DOMContentLoaded', function() {
   var randomMessages = document.getElementsByClassName('random-message');
   for (let i = 0; i < randomMessages.length; i++) {
     var message = randomMessages[i]
-    if(Math.round(date / 1000 / 60 / 5) % Math.round(1 / message.dataset.displayProbability) == 0) { // divide time since epoch by slots of 5 minutes
+    var offset = (message.dataset.displayOffset == null) ? 0 : Number(message.dataset.displayOffset);
+    if((Math.round(date / 1000 / 60 / 5) + offset) % Math.round(1 / message.dataset.displayProbability) == 0) { // divide time since epoch by slots of 5 minutes
       message.style.display = "block";
       if(message.id == "donate") {
         document.documentElement.dataset.hideSidebarDonate = "true";
