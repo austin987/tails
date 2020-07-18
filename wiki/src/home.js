3db9cf6e48fb1538c00398aca614fe082ced7311
sajolida <sajolida@pimienta.org>
2020-07-18 02:37:00 +0000

    Change the randomness to be time-based instead of display-based
    I think that it maps better a single user session.
    
    For example, for surveys:
    
    - People might click on a link and go back to /home: we want the message
      to stick.
    - People might refresh several times to get the link to appear again and
      answer the survey twice: we won't want to message to appear again so
      easily.

--- wiki/src/home.js
+++ wiki/src/home.js
@@ -2,10 +2,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
   document.title = "Tails";
 
+  var date = Date.now()
   var randomMessages = document.getElementsByClassName('random-message');
   for (let i = 0; i < randomMessages.length; i++) {
     var message = randomMessages[i]
-    if(Math.floor(1/message.dataset.displayProbability * Math.random()) == 0) {
+    if(Math.round(date / 1000 / 60 / 5) % Math.round(1 / message.dataset.displayProbability) == 0) { // divide time since epoch by slots of 5 minutes
       message.style.display = "block";
       if(message.id == "donate") {
         document.documentElement.dataset.hideSidebarDonate = "true";
