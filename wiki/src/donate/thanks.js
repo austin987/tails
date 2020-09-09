06adf5bf42741483f8aa9679cdb411633c3df534
sajolida <sajolida@pimienta.org>
2020-09-09 15:07:31 +0000

    Be personal
    I haven't tested this yet using a real donation because I suspect that
    we don't have Auto Return enabled:
    
    https://developer.paypal.com/docs/admin/checkout-settings/#auto-return-the-buyer-to-your-website

---
+++ wiki/src/donate/thanks.js
@@ -0,0 +1,13 @@
+// The customization of /donate/thanks can be tested using using /donate/thanks/test.
+
+// Store the GET parameters returned by PayPal even before the page finished to launch
+params = new URLSearchParams(document.location.search.substring(1));
+
+// Hide the GET parameters returned by PayPal
+window.history.replaceState(null, "donate", window.location.href.split('?')[0]);
+
+document.addEventListener('DOMContentLoaded', function() {
+  if (params.has("first_name")) {
+    document.getElementById("name").innerHTML = params.get("first_name");
+  }
+});
