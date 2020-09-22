austin987/tails

commit df8e1f3569ffd79dbf693d00f3ec89d2e78a80f3

IUK creation: instrument lengthy operations
Loop-mounting costs seem to be very close to 0, and attribute-related
fixups are negligible compared to the rsync/mksquashfs operations, so
concentrate on those only.

config/chroot_local-includes/usr/src/iuk/lib/Tails/IUK.pm
@@ -337,6 +337,8 @@ method create_squashfs_diff () {
         );
     }
 
+    my $basename = path($self->outfile)->basename;
+    my $t1 = time;
     my @rsync_options = qw{--archive --quiet --delete-after --acls --checksum};
     push @rsync_options, "--xattrs" if $self->union_type eq 'overlayfs';
     run_as_root(
@@ -344,6 +346,8 @@ method create_squashfs_diff () {
         sprintf("%s/", $new_squashfs_mount),
         sprintf("%s/", $union_mount),
     );
+    my $t2 = time;
+    printf "TIME (rsync for $basename): %d seconds\n", ($t2-$t1);
 
     for my $glob (@{$self->ignore_if_same_content}) {
         my @candidates_for_removal = map {
@@ -402,6 +406,7 @@ method create_squashfs_diff () {
         }
     }
 
+    $t1 = time;
     run_as_root(
         "SOURCE_DATE_EPOCH=$ENV{SOURCE_DATE_EPOCH}",
         qw{mksquashfs},
@@ -409,6 +414,8 @@ method create_squashfs_diff () {
         $self->overlay_dir->child('live', $self->squashfs_diff_name),
         $self->list_mksquashfs_options
     );
+    $t2 = time;
+    printf "TIME (main mksquashfs for $basename): %d seconds\n", ($t2-$t1);
 
     foreach ($union_basedir,
              $new_squashfs_mount, $new_iso_mount,
@@ -452,6 +459,8 @@ method saveas ($outfile_name) {
 
     $self->prepare_overlay_dir;
 
+    my $basename = path($self->outfile)->basename;
+    my $t1 = time;
     run_as_root(
         "SOURCE_DATE_EPOCH=$ENV{SOURCE_DATE_EPOCH}",
         qw{mksquashfs},
@@ -460,6 +469,8 @@ method saveas ($outfile_name) {
         $self->list_mksquashfs_options,
         '-all-root',
     );
+    my $t2 = time;
+    printf "TIME (final mksquashfs for $basename): %d seconds\n", ($t2-$t1);
 
     return;
 }

commit 53fc078853341576cd314bbd2d978b7eabc21dc5

IUK creation: make saveas method benchmarking info honor its $outfile_name argument
This fixes t/specs/Create.t, which was broken by
df8e1f3569ffd79dbf693d00f3ec89d2e78a80f3: the `saveas` method shall not rely on
$self->outfile, but instead honor its $outfile_name argument, which matters for
unit testing. The `save` method, which is used by tails-create-iuk, will forward
the requested $self->outfile to the `saveas` method.

Similarly, the create_squashfs_diff method shall not rely on $self->outfile
being set.

config/chroot_local-includes/usr/src/iuk/lib/Tails/IUK.pm
@@ -349,14 +349,15 @@ method create_squashfs_diff () {
     my @rsync_options = qw{--archive --quiet --delete-after --acls --checksum
                            --xattrs};
 
-    my $basename = path($self->outfile)->basename;
     my $t1 = time;
     run_as_root(
         "rsync", @rsync_options,
         sprintf("%s/", $new_squashfs_mount),
         sprintf("%s/", $union_mount),
     );
-    printf "TIME (rsync for $basename): %d seconds\n", (time - $t1);
+    printf "TIME (rsync for %s): %d seconds\n",
+        $self->squashfs_diff_name,
+        (time - $t1);
 
     for my $glob (@{$self->ignore_if_same_content}) {
         my @candidates_for_removal = map {
@@ -418,7 +419,9 @@ method create_squashfs_diff () {
         $self->list_mksquashfs_options,
         qw{-Xbcj x86 -b 1024K -Xdict-size 1024K},
     );
-    printf "TIME (main mksquashfs for $basename): %d seconds\n", (time - $t1);
+    printf "TIME (main mksquashfs for %s): %d seconds\n",
+        $self->squashfs_diff_name,
+        (time - $t1);
 
     foreach ($union_basedir,
              $new_squashfs_mount, $new_iso_mount,
@@ -462,7 +465,6 @@ method saveas ($outfile_name) {
 
     $self->prepare_overlay_dir;
 
-    my $basename = path($self->outfile)->basename;
     my $t1 = time;
     run_as_root(
         $self->list_mksquashfs_prefix_cmd,
@@ -472,7 +474,9 @@ method saveas ($outfile_name) {
         $self->list_mksquashfs_options,
         '-all-root',
     );
-    printf "TIME (final mksquashfs for $basename): %d seconds\n", (time - $t1);
+    printf "TIME (final mksquashfs for %s): %d seconds\n",
+        path($outfile_name)->basename,
+        (time - $t1);
 
     return;
 }