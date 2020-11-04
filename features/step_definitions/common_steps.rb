austin987/tails

commit 7b1e2567679fde467c27c3d8841228ac98cd8767

Test suite: login using the button, not accelerators.
This allows us to drop the start_accel column in the scenario outline,
which is nice to not have to maintain.

features/step_definitions/common_steps.rb
@@ -344,14 +344,23 @@ Given /^I set the language to (.*)$/ do |lang|
   @screen.press('Return')
 end
 
-Given /^I log in to a new session(?: in (.*) with accelerator "([^"]+)")?$/ do |lang, start_accel|
-  start_accel ||= 's'
+Given /^I log in to a new session(?: in (.*)?)$/ do |lang|
+  # We'll record the location of the login button before changing
+  # language so we only need one (English) image for the button while
+  # still being able to click it in any language.
+  if RTL_LANGUAGES.include?(lang)
+    # If we select a RTL language below, the login and shutdown
+    # buttons will swap place.
+    login_button_region = @screen.find('TailsGreeterShutdownButton.png')
+  else
+    login_button_region = @screen.find('TailsGreeterLoginButton.png')
+  end
   step "I set the language to #{lang}" if lang && lang != 'English'
   # After selecting options (language, administration password, etc.),
   # the Greeter needs some time to focus the main window back, so that
   # typing the accelerator for the "Start Tails" button is honored.
   sleep(1)
-  @screen.press('alt', start_accel)
+  login_button_region.click
   step 'the Tails desktop is ready'
 end
 

features/localization.feature
@@ -7,7 +7,7 @@ Feature: Localization
   @doc
   Scenario: The Report an Error launcher opens the support documentation in supported non-English locales
     Given I have started Tails from DVD without network and stopped at Tails Greeter's login screen
-    And I log in to a new session in German with accelerator "s"
+    And I log in to a new session in German
     When I double-click on the Report an Error launcher on the desktop
     Then the support documentation page opens in Tor Browser
 
@@ -21,7 +21,7 @@ Feature: Localization
   @fragile
   Scenario Outline: Tails is localized for every tier-1 language
     Given I have started Tails from DVD without network and stopped at Tails Greeter's login screen
-    When I log in to a new session in <language> with accelerator "<start_accel>"
+    When I log in to a new session in <language>
     Then the keyboard layout is set to "<layout>"
     When the network is plugged
     And Tor is ready
@@ -38,17 +38,17 @@ Feature: Localization
     #  - Not all localized layouts exist in the GNOME screen keyboard: #8444
     #  - Arabic's layout should be "ara": #12638
     Examples:
-      | language   | start_accel | layout | osk_layout |
-      | Arabic     | s           | us     | us         |
-      | Chinese    | s           | cn     | us         |
-      | English    | s           | us     | us         |
-      | French     | d           | fr     | fr         |
-      | German     | s           | de     | de         |
-      | Hindi      | s           | in     | us         |
-      | Indonesian | s           | id     | us         |
-      | Italian    | s           | it     | us         |
-      | Persian    | s           | ir     | ir         |
-      | Portuguese | s           | pt     | us         |
-      | Russian    | s           | ru     | ru         |
-      | Spanish    | t           | es     | us         |
-      | Turkish    | b           | tr     | us         |
+      | language   | layout | osk_layout |
+      | Arabic     | us     | us         |
+      | Chinese    | cn     | us         |
+      | English    | us     | us         |
+      | French     | fr     | fr         |
+      | German     | de     | de         |
+      | Hindi      | in     | us         |
+      | Indonesian | id     | us         |
+      | Italian    | it     | us         |
+      | Persian    | ir     | ir         |
+      | Portuguese | pt     | us         |
+      | Russian    | ru     | ru         |
+      | Spanish    | es     | us         |
+      | Turkish    | tr     | us         |

features/support/config.rb
@@ -84,6 +84,7 @@ SERVICES_EXPECTED_ON_ALL_IFACES =
   ].freeze
 # OpenDNS
 SOME_DNS_SERVER = '208.67.222.222'.freeze
+RTL_LANGUAGES = ['Arabic', 'Persian'].freeze
 VM_XML_PATH = "#{Dir.pwd}/features/domains".freeze
 
 TAILS_SIGNING_KEY = cmd_helper(

commit 33103b2b978e3ef413b36c8858acab69c9a009c2

Test suite: fix regexp
Fix regression introduced by 7b1e2567679fde467c27c3d8841228ac98cd8767,
which breaks all scenarios where we log in in English:

  Undefined dynamic step: "I log in to a new session" (Cucumber::UndefinedDynamicStep)

features/step_definitions/common_steps.rb
@@ -344,7 +344,7 @@ Given /^I set the language to (.*)$/ do |lang|
   @screen.press('Return')
 end
 
-Given /^I log in to a new session(?: in (.*)?)$/ do |lang|
+Given /^I log in to a new session(?: in (.*))?$/ do |lang|
   # We'll record the location of the login button before changing
   # language so we only need one (English) image for the button while
   # still being able to click it in any language.