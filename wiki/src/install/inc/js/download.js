4300e663c4e8a8a173412cf58853e503174eef5b
sajolida <sajolida@pimienta.org>
2020-11-10 01:19:53 +0000

    Display a deprecation notice when the extension is installed
    In theory, it should also be possible to publish an update that
    uninstalls itself:
    
    https://extensionworkshop.com/documentation/manage/retiring-your-extension/
    
    But I couldn't get it to work. Not publishing any update will also save
    us time.

--- wiki/src/install/inc/js/download.js
+++ wiki/src/install/inc/js/download.js
@@ -1,5 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+  /* Deprecation of the extension */
+
+  window.addEventListener("message", receiveMessage);
+  function receiveMessage(event) {
+    if (event.source !== window || event.origin !== "https://tails.boum.org" || !event.data) {
+      return;
+    }
+    if (event.data.action === "extension-installed") {
+      show(document.getElementById("extension"));
+    }
+  }
+
   //specify url of json file containing valid checksums for ISO and USB images below:
   //var URLofJsonFileContainingChecksums='https://tails.boum.org/install/v2/Tails/amd64/stable/latest.json';  						//this URL will only work if download.html is served from  served from https://tails.boum.org/ because of same origin because of same-origin policy.
   var URLofJsonFileContainingChecksums='https://cors-anywhere.herokuapp.com/https://tails.boum.org/install/v2/Tails/amd64/stable/latest.json';  		//use this URL to get around same-origin policy (SOP) if you are staging this locally.  The https://cors-anywhere.herokuapp.com/ proxy includes the necessary CORS headers to relax SOP.
