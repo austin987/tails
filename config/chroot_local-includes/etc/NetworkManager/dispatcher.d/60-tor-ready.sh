01693f91a3a5f238a4008a18aa3e684d8fd7ecf9
anonym <anonym@riseup.net>
2021-02-02 15:38:22 +0100

    Drop ugly hook killing Tor Launcher.
    Now that onion-grater transparently (for the clients) deal with lost
    tor controller connections, Tor Launcher will never lose track of the
    bootstrap progress and will quit by itself.

--- config/chroot_local-includes/etc/NetworkManager/dispatcher.d/60-tor-ready.sh
+++ config/chroot_local-includes/etc/NetworkManager/dispatcher.d/60-tor-ready.sh
@@ -26,18 +26,6 @@ while ! tor_has_bootstrapped; do
    sleep 1
 done
 
-# We now know that whatever Tor settings we are using works, so if Tor
-# Launcher is still running, we can just kill it and make sure it
-# won't start next network reconnect. A reason for this happening is
-# if Tor was restarted by tordate, e.g. if the clock was to incorrect.
-TOR_LAUNCHER_PROCESS_REGEX="firefox-unconfined -?-app.*tor-launcher-standalone"
-if pgrep -f "${TOR_LAUNCHER_PROCESS_REGEX}"; then
-   pkill -f "${TOR_LAUNCHER_PROCESS_REGEX}"
-   pref=/home/tor-launcher/.tor-launcher/profile.default/prefs.js
-   sed -i '/^user_pref("extensions\.torlauncher\.prompt_at_startup"/d' "${pref}"
-   echo 'user_pref("extensions.torlauncher.prompt_at_startup", false);' >> "${pref}"
-fi
-
 /usr/local/sbin/tails-notify-user \
    "`gettext \"Tor is ready\"`" \
    "`gettext \"You can now access the Internet.\"`"
