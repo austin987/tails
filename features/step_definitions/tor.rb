014af7fa379b0aa2c7ef488a30ccc6e355b563a5
anonym <anonym@riseup.net>
2021-03-30 11:23:36 +0200

    Revert "Test suite: wait for TCA's GUI to be available before using it."
    This reverts commit 7ddaecbdde2fbaa708769f31c3e2e0499b4e3ddd.
    
    Turns out this didn't do anything.

--- features/step_definitions/tor.rb
+++ features/step_definitions/tor.rb
@@ -340,8 +340,6 @@ When /^the Tor Connection Assistant autostarts$/ do
   try_for(60) do
     @tor_connection_assistant = Dogtail::Application.new('TorConnectionAssistant')
   end
-  # Wait for the GUI to be showing before proceeding
-  tor_connection_assistant.child('Connect to Tor', roleName: 'push button')
 end
 
 def tor_connection_assistant
