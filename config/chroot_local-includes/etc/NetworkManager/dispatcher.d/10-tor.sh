660238466efed07f58b5b5bd38b020cd0b6da127
boyska <boyska@riseup.net>
2021-06-15 16:20:45 +0200

    wait for user systemd session to be ready

--- config/chroot_local-includes/etc/NetworkManager/dispatcher.d/10-tor.sh
+++ config/chroot_local-includes/etc/NetworkManager/dispatcher.d/10-tor.sh
@@ -36,6 +36,13 @@ rm -f "${TOR_LOG}"
 # To work around this we restart Tor.
 systemctl restart tor@default.service
 
+while ! pgrep --euid amnesia -x gnome-shell; do
+    sleep 1
+done
+while ! systemctl is-active -q user@1000.service; do
+    sleep 1
+done
+
 /usr/local/lib/systemctl-user amnesia start tca.service
 
 # Wait until the user has done the Tor Launcher configuration.
